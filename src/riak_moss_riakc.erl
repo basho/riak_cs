1a1e048557e670ea898fa0926854d9b156b906e9
Reid Draper <reiddraper@gmail.com>
2011-10-13 14:59:16 -0400

    Change the order of methods so they line up
    AZ845

--- src/riak_moss_riakc.erl
+++ src/riak_moss_riakc.erl
@@ -68,14 +68,14 @@ create_bucket(KeyID, BucketName) ->
 delete_bucket(KeyID, BucketName) ->
     gen_server:call(?MODULE, {delete_bucket, KeyID, BucketName}).
 
+get_object(BucketName, Key) ->
+    gen_server:call(?MODULE, {get_object, BucketName, Key}).
+
 %% TODO:
 %% What are we actually doing with
 %% the KeyID?
 put_object(KeyID, BucketName, Key, Val, Metadata) ->
     gen_server:call(?MODULE, {put_object, KeyID, BucketName, Key, Val, Metadata}).
-
-get_object(BucketName, Key) ->
-    gen_server:call(?MODULE, {get_object, BucketName, Key}).
 
 handle_call({get_user, KeyID}, _From, State=#state{riakc_pid=RiakcPid}) ->
     {reply, do_get_user(KeyID, RiakcPid), State};
@@ -92,12 +92,12 @@ handle_call({create_bucket, KeyID, BucketName}, _From, State=#state{riakc_pid=Ri
 handle_call({delete_bucket, KeyID, BucketName}, _From, State=#state{riakc_pid=RiakcPid}) ->
     {reply, do_delete_bucket(KeyID, BucketName, RiakcPid), State};
 
+handle_call({get_object, BucketName, Key}, _From, State=#state{riakc_pid=RiakcPid}) ->
+    {reply, do_get_object(BucketName, Key, RiakcPid), State};
+
 handle_call({put_object, KeyID, BucketName, Key, Value, Metadata},
                    _From, State=#state{riakc_pid=RiakcPid}) ->
-    {reply, do_put_object(KeyID, BucketName, Key, Value, Metadata, RiakcPid), State};
-
-handle_call({get_object, BucketName, Key}, _From, State=#state{riakc_pid=RiakcPid}) ->
-    {reply, do_get_object(BucketName, Key, RiakcPid), State}.
+    {reply, do_put_object(KeyID, BucketName, Key, Value, Metadata, RiakcPid), State}.
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -116,6 +116,14 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal functions
 %% ====================================================================
 
+do_get_user(KeyID, RiakcPid) ->
+    case riakc_pb_socket:get(RiakcPid, ?USER_BUCKET, list_to_binary(KeyID)) of
+        {ok, Obj} ->
+            {ok, binary_to_term(riakc_obj:get_value(Obj))};
+        Error ->
+            Error
+    end.
+
 do_create_user(UserName, RiakcPid) ->
     %% TODO: Is it outside the scope
     %% of this module for this func
@@ -126,6 +134,11 @@ do_create_user(UserName, RiakcPid) ->
     User = #rs3_user{name=UserName, key_id=KeyID, key_secret=Secret},
     do_save_user(User, RiakcPid),
     User.
+
+do_save_user(User, RiakcPid) ->
+    UserObj = riakc_obj:new(?USER_BUCKET, list_to_binary(User#rs3_user.key_id), User),
+    ok = riakc_pb_socket:put(RiakcPid, UserObj),
+    ok.
 
 do_get_buckets(KeyID, RiakcPid) ->
     User = do_get_user(KeyID, RiakcPid),
@@ -173,6 +186,14 @@ do_delete_bucket(KeyID, BucketName, RiakcPid) ->
     UpdatedUser = User#rs3_user{buckets=UpdatedBuckets},
     do_save_user(UpdatedUser, RiakcPid).
 
+do_get_object(BucketName, Key, RiakcPid) ->
+    %% TODO:
+    %% Should we be converting the
+    %% key to binary here, or in the
+    %% the public api method?
+    BinKey = list_to_binary(Key),
+    riakc_pb_socket:get(RiakcPid, BucketName, BinKey).
+
 do_put_object(KeyID, BucketName, Key, Value, Metadata, RiakcPid) ->
     %% TODO: KeyID is currently
     %% not used
@@ -185,24 +206,3 @@ do_put_object(KeyID, BucketName, Key, Value, Metadata, RiakcPid) ->
     RiakObject = riakc_obj:new(BucketName, BinKey, Value),
     NewObj = riak_obj:update_metadata(RiakObject, Metadata),
     riakc_pb_socket:put(RiakcPid, NewObj).
-
-do_get_object(BucketName, Key, RiakcPid) ->
-    %% TODO:
-    %% Should we be converting the
-    %% key to binary here, or in the
-    %% the public api method?
-    BinKey = list_to_binary(Key),
-    riakc_pb_socket:get(RiakcPid, BucketName, BinKey).
-
-do_save_user(User, RiakcPid) ->
-    UserObj = riakc_obj:new(?USER_BUCKET, list_to_binary(User#rs3_user.key_id), User),
-    ok = riakc_pb_socket:put(RiakcPid, UserObj),
-    ok.
-
-do_get_user(KeyID, RiakcPid) ->
-    case riakc_pb_socket:get(RiakcPid, ?USER_BUCKET, list_to_binary(KeyID)) of
-        {ok, Obj} ->
-            {ok, binary_to_term(riakc_obj:get_value(Obj))};
-        Error ->
-            Error
-    end.
