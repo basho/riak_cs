f29cc82f73942188cfb085b5d3b498ecf201cfa1
Kelly McLaughlin <kelly@basho.com>
2011-10-17 09:19:56 -0600

    Revert "Remove obsolete blockall auth module."
    This reverts commit 36897ebc1728a044cdb988bdd26c017965db3a5f.

---
+++ src/riak_moss_blockall_auth.erl
@@ -0,0 +1,23 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2007-2011 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% -------------------------------------------------------------------
+
+-module(riak_moss_blockall_auth).
+
+-behavior(riak_moss_auth).
+
+-include("riak_moss.hrl").
+
+-export([authenticate/1]).
+
+-spec authenticate(term()) -> {ok, #rs3_user{}}
+                        | {ok, unknown}
+                        | {error, atom()}.
+authenticate(_RD) ->
+    %% TODO:
+    %% is there a better
+    %% atom we could return?
+    {error, invalid_authentication}.
+
