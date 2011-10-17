e712172e7de6ad5e2f978632443594d8a9196544
Reid Draper <reiddraper@gmail.com>
2011-10-17 19:29:27 -0400

    Match an empty list argument for authenticate first
    AZ871

--- src/riak_moss_passthru_auth.erl
+++ src/riak_moss_passthru_auth.erl
@@ -15,19 +15,18 @@
 -spec authenticate(term(), [string()]) -> {ok, #rs3_user{}}
                                               | {ok, unknown}
                                               | {error, atom()}.
-authenticate(_RD, KeyID) when is_list(KeyID) ->
-    case riak_moss_riakc:get_user(KeyID) of
-        {ok, User} ->
-            {ok, User};
-        _ ->
-            {error, invalid_authentication}
-    end;
-authenticate(_RD, _AuthArgs) ->
+authenticate(_RD, []) ->
     %% TODO:
     %% Even though we're
     %% going to authorize
     %% no matter what,
     %% maybe it still makes sense
     %% to pass the user on
-    {ok, unknown}.
-
+    {ok, unknown};
+authenticate(_RD, KeyID) when is_list(KeyID) ->
+    case riak_moss_riakc:get_user(KeyID) of
+        {ok, User} ->
+            {ok, User};
+        _ ->
+            {error, invalid_authentication}
+    end.
