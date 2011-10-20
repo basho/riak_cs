28ec780c26d55844a7a222ca3e040f22f5cb5db1
Andy Gross <andy@andygross.org>
2011-10-20 11:15:32 -0700

    test auth, use random data and key/bucket names

--- tests/prototype_parity.py
+++ tests/prototype_parity.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 
+from boto.exception import S3ResponseError
 from boto.s3.connection import S3Connection, OrdinaryCallingFormat
 from boto.s3.key import Key
-import unittest, time
+import unittest, time, uuid
 
 KEY_ID="0"
 SECRET_KEY="9372f58b69b05001"
 HOST="127.0.0.1"
 PORT=80
-data = file("/dev/random").read(1024)
-
 
 class ParityWithPrototypeTest(unittest.TestCase):
-    def setUp(self):
-        self.bucket_name = "parity-test"
-        self.key_name = "parity-test"
-        self.conn = S3Connection(KEY_ID, SECRET_KEY, is_secure=False, 
-                                 host=HOST, port=PORT, debug=True, 
-                                 calling_format=OrdinaryCallingFormat())
 
+    def make_connection(self, key_id=KEY_ID, secret_key=SECRET_KEY):
+        return S3Connection(key_id, secret_key, is_secure=False, 
+                            host=HOST, port=PORT, debug=True, 
+                            calling_format=OrdinaryCallingFormat() )       
+    @classmethod
+    def setUpClass(self):
+        self.bucket_name = str(uuid.uuid4())
+        self.key_name = str(uuid.uuid4())
+        self.data = file("/dev/random").read(1024)
+
+    def setUp(self):
+        self.conn = self.make_connection()
+
+    def test_0_auth(self):
+        conn = self.make_connection("BAD_ID", "BAD_KEY")
+        self.assertRaises(S3ResponseError, conn.get_canonical_user_id)
 
     def test_1_create_bucket(self):
         self.conn.create_bucket(self.bucket_name)
@@ -31,13 +40,13 @@ class ParityWithPrototypeTest(unittest.TestCase):
         bucket = self.conn.get_bucket(self.bucket_name)
         k = Key(bucket)
         k.key = self.key_name
-        k.set_contents_from_string("parity-test")
+        k.set_contents_from_string(self.data)
 
     def test_4_get_object(self):
         bucket = self.conn.get_bucket(self.bucket_name)
         k = Key(bucket)
         k.key = self.key_name        
-        self.assertEqual(k.get_contents_as_string(), "parity-test")
+        self.assertEqual(k.get_contents_as_string(), self.data)
 
     def test_5_list_bucket(self):
         bucket = self.conn.get_bucket(self.bucket_name)
@@ -62,7 +71,8 @@ class ParityWithPrototypeTest(unittest.TestCase):
 
     def test_9_list_buckets(self):
         self.assertNotIn(self.bucket_name, 
-                         [b.name for b in self.conn.get_all_buckets()])                
+                         [b.name for b in self.conn.get_all_buckets()]) 
+               
 
 if __name__ == "__main__":
     unittest.main()
