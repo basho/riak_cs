afbe5391041ab7193aac112c5c0b716a3e3ed27f
Reid Draper <reiddraper@gmail.com>
2011-11-03 16:07:05 -0400

    Add skeleton new and run funcs
    AZ918

--- bench/basho_bench_driver_moss.erl
+++ bench/basho_bench_driver_moss.erl
@@ -3,3 +3,20 @@
 %% Copyright (c) 2007-2011 Basho Technologies, Inc.  All Rights Reserved.
 %%
 %% -------------------------------------------------------------------
+
+-module(basho_bench_driver_moss).
+
+-export([new/1,
+         run/4]).
+
+-include("basho_bench.hrl").
+
+-record(state, {}).
+
+-spec new(integer()) -> {ok, term()}.
+new(_ID) ->
+    {ok, #state{}}.
+
+-spec run(atom(), fun(), fun(), term()) -> {ok, term()}.
+run(_Operation, _KeyGen, _ValueGen, State) ->
+    {ok, State}.
