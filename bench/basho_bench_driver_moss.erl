293eb3281f723e70e45b159f78a77ab0178b9143
Reid Draper <reiddraper@gmail.com>
2011-11-04 16:48:37 -0400

    Borrow and edit code from http_raw driver
    AZ918

--- bench/basho_bench_driver_moss.erl
+++ bench/basho_bench_driver_moss.erl
@@ -36,12 +36,24 @@ new(ID) ->
     {ok, #state{client_id=ID, hosts=Hosts}}.
 
 -spec run(atom(), fun(), fun(), term()) -> {ok, term()}.
+run(insert, KeyGen, ValueGen, State) ->
+    {NextURL, S2} = next_host(State),
+    {ok, S2};
+
 run(_Operation, _KeyGen, _ValueGen, State) ->
     {ok, State}.
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+-spec next_host(term()) -> {term(), term()}.
+%% TODO:
+%% Currently we only support
+%% one host, so just return
+%% that
+next_host(State=#state{hosts=Hosts}) ->
+    [Host] = Hosts,
+    {Host, State}.
 
 %% This is ripped from basho_bench_driver_http_raw.erl
 connect(Url) ->
@@ -71,3 +83,82 @@ disconnect(Url) ->
     end,
     erlang:erase({ibrowse_pid, Url}),
     ok.
+
+maybe_disconnect(Url) ->
+    case erlang:get(disconnect_freq) of
+        infinity -> ok;
+        {ops, Count} -> should_disconnect_ops(Count,Url) andalso disconnect(Url);
+        Seconds -> should_disconnect_secs(Seconds,Url) andalso disconnect(Url)
+    end.
+
+should_disconnect_ops(Count, #url{host=Host, port=Port}) ->
+    Key = {ops_since_disconnect, {Host, Port}},
+    case erlang:get(Key) of
+        undefined ->
+            erlang:put(Key, 1),
+            false;
+        Count ->
+            erlang:put(Key, 0),
+            true;
+        Incr ->
+            erlang:put(Key, Incr + 1),
+            false
+    end.
+
+should_disconnect_secs(Seconds, #url{host=Host, port=Port}) ->
+    Key = {last_disconnect, {Host, Port}},
+    case erlang:get(Key) of
+        undefined ->
+            erlang:put(Key, erlang:now()),
+            false;
+        Time when is_tuple(Time) andalso size(Time) == 3 ->
+            Diff = timer:now_diff(erlang:now(), Time),
+            if
+                Diff >= Seconds * 1000000 ->
+                    erlang:put(Key, erlang:now()),
+                    true;
+                true -> false
+            end
+    end.
+
+clear_disconnect_freq(Url) ->
+    case erlang:get(disconnect_freq) of
+        infinity -> ok;
+        {ops, _Count} -> erlang:put({ops_since_disconnect, Url#url.host}, 0);
+        _Seconds -> erlang:put({last_disconnect, Url#url.host}, erlang:now())
+    end.
+
+send_request(Url, Headers, Method, Body, Options) ->
+    send_request(Url, Headers, Method, Body, Options, 3).
+
+send_request(_Url, _Headers, _Method, _Body, _Options, 0) ->
+    {error, max_retries};
+send_request(Url, Headers, Method, Body, Options, Count) ->
+    Pid = connect(Url),
+    case catch(ibrowse_http_client:send_req(Pid, Url, Headers, Method, Body, Options, basho_bench_config:get(moss_request_timeout, 5000))) of
+        {ok, Status, RespHeaders, RespBody} ->
+            maybe_disconnect(Url),
+            {ok, Status, RespHeaders, RespBody};
+
+        Error ->
+            clear_disconnect_freq(Url),
+            disconnect(Url),
+            case should_retry(Error) of
+                true ->
+                    send_request(Url, Headers, Method, Body, Options, Count-1);
+
+                false ->
+                    normalize_error(Method, Error)
+            end
+    end.
+
+
+should_retry({error, send_failed})       -> true;
+should_retry({error, connection_closed}) -> true;
+should_retry({'EXIT', {normal, _}})      -> true;
+should_retry({'EXIT', {noproc, _}})      -> true;
+should_retry(_)                          -> false.
+
+normalize_error(Method, {'EXIT', {timeout, _}})  -> {error, {Method, timeout}};
+normalize_error(Method, {'EXIT', Reason})        -> {error, {Method, 'EXIT', Reason}};
+normalize_error(Method, {error, Reason})         -> {error, {Method, Reason}}.
