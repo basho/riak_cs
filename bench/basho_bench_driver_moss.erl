70c75447a9598841a5225329ae53bcb1bf642118
Reid Draper <reiddraper@gmail.com>
2011-11-04 10:43:38 -0400

    Add connect and disconnect funcs from Riak http driver
    AZ918

--- bench/basho_bench_driver_moss.erl
+++ bench/basho_bench_driver_moss.erl
@@ -11,7 +11,12 @@
 
 -include("basho_bench.hrl").
 
+-record(url, {host, port}).
 -record(state, { client_id }).
+
+%% ====================================================================
+%% API
+%% ====================================================================
 
 -spec new(integer()) -> {ok, term()}.
 new(ID) ->
@@ -21,3 +26,36 @@ new(ID) ->
 -spec run(atom(), fun(), fun(), term()) -> {ok, term()}.
 run(_Operation, _KeyGen, _ValueGen, State) ->
     {ok, State}.
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+%% This is ripped from basho_bench_driver_http_raw.erl
+connect(Url) ->
+    case erlang:get({ibrowse_pid, Url#url.host}) of
+        undefined ->
+            {ok, Pid} = ibrowse_http_client:start({Url#url.host, Url#url.port}),
+            erlang:put({ibrowse_pid, Url#url.host}, Pid),
+            Pid;
+        Pid ->
+            case is_process_alive(Pid) of
+                true ->
+                    Pid;
+                false ->
+                    erlang:erase({ibrowse_pid, Url#url.host}),
+                    connect(Url)
+            end
+    end.
+
+
+%% This is ripped from basho_bench_driver_http_raw.erl
+disconnect(Url) ->
+    case erlang:get({ibrowse_pid, Url#url.host}) of
+        undefined ->
+            ok;
+        OldPid ->
+            catch(ibrowse_http_client:stop(OldPid))
+    end,
+    erlang:erase({ibrowse_pid, Url#url.host}),
+    ok.
