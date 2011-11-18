b4c84ff046033e1519e4fc359f5c789db77650b4
Reid Draper <reiddraper@gmail.com>
2011-11-18 17:54:03 -0500

    Eliminate compiler warnings, add rest of gen_fsm behaviours
    AZ940

--- src/riak_moss_get_fsm.erl
+++ src/riak_moss_get_fsm.erl
@@ -10,17 +10,23 @@
 
 -behaviour(gen_fsm).
 
+-export([init/1, handle_event/3, handle_sync_event/4,
+         handle_info/3, terminate/3, code_change/4]).
+
 -export([start_link/3,
-         init/1,
          prepare/2,
          waiting_value/2,
+         retriever/3,
          waiting_chunks/2]).
 
 start_link(From, Bucket, Key) ->
     gen_fsm:start_link(?MODULE, [From, Bucket, Key], []).
 
 init([From, Bucket, Key]) ->
-    State = From,
+    %% TODO:
+    %% turn this into a real
+    %% record
+    State = {From, Bucket, Key},
     %% purposely have the timeout happen
     %% so that we get called in the prepare
     %% state
@@ -29,10 +35,11 @@ init([From, Bucket, Key]) ->
 %% TODO:
 %% could this func use
 %% use a better name?
-prepare(timeout, State) ->
+prepare(timeout, {_ReplyPid, Bucket, Key}=State) ->
     %% start the process that will
     %% fetch the value, be it manifest
     %% or regular object
+    spawn_link(?MODULE, retriever, [self(), Bucket, Key]),
     {next_state, waiting_value, State}.
 
 waiting_value({object, Value}, State) ->
@@ -70,22 +77,45 @@ waiting_chunks({chunk, Chunk}, State) ->
 %%      object, return it. If it's a manifest,
 %%      start grabbing the chunks and returning them.
 retriever(ReplyPid, Bucket, Key) ->
-    ok.
+    gen_fsm:send_event(ReplyPid,
+        riak_moss_riakc:get_object(Bucket, Key)).
 
 %% @doc Returns whether or not
 %%      a value is a normal object,
 %%      or a manifest document
-object_or_manifest(Value) ->
+object_or_manifest(_Value) ->
     ok.
 
 %% @doc Remove a chunk from the
 %%      chunks field of State
-remove_chunk(State, Chunk) ->
+remove_chunk(_State, _Chunk) ->
     ok.
 
 %% @doc Return true or false
 %%      depending on whether
 %%      we're still waiting
 %%      to accumulate more chunks
-still_waiting(State) ->
+still_waiting(_State) ->
     ok.
+
+%% @private
+handle_event(_Event, _StateName, StateData) ->
+    {stop,badmsg,StateData}.
+
+%% @private
+handle_sync_event(_Event, _From, _StateName, StateData) ->
+    {stop,badmsg,StateData}.
+
+%% @private
+handle_info(request_timeout, StateName, StateData) ->
+    ?MODULE:StateName(request_timeout, StateData);
+%% @private
+handle_info(_Info, _StateName, StateData) ->
+    {stop,badmsg,StateData}.
+
+%% @private
+terminate(Reason, _StateName, _State) ->
+    Reason.
+
+%% @private
+code_change(_OldVsn, StateName, State, _Extra) -> {ok, StateName, State}.
