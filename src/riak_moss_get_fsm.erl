8020206b906950c0b62a2dfff0a6da6f51f87b61
Reid Draper <reiddraper@gmail.com>
2011-11-18 15:33:49 -0500

    First work on a skeleton for the get fsm
    AZ940

---
+++ src/riak_moss_get_fsm.erl
@@ -0,0 +1,77 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2007-2011 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% -------------------------------------------------------------------
+
+%% get fsm for Riak Moss. 
+
+-module(riak_moss_get_fsm).
+
+-behaviour(gen_fsm).
+
+-export([start_link/3, init/1, chunk/1, return/0]).
+
+start_link(From, Bucket, Key) ->
+    gen_fsm:start_link(?MODULE, [From, Bucket, Key], []).
+
+init([From, Bucket, Key]) ->
+    %% start the process that will
+    %% fetch the value, be it manifest
+    %% or regular object
+    State = From,
+    {ok, waiting_value, State}.
+
+waiting_value({object, Value}, State) ->
+    %% determine if the object is a normal
+    %% object, or a manifest object
+    case object_or_manifest(Value) of
+        object ->
+            %% send object back to
+            %% the `from` part of
+            %% state
+            {stop, done, State};
+        manifest ->
+            %% now launch a process that
+            %% will grab the chunks and
+            %% start sending us
+            %% chunk events
+            {ok, waiting_chunks, State}
+    end.
+
+waiting_chunks({chunk, Chunk}, State) ->
+    %% we're assuming that we're receiving the
+    %% chunks synchronously, and that we can
+    %% send them back to WM as we get them
+    NewState = remove_chunk(State, Chunk),
+    case still_waiting(NewState) of
+        true ->
+            {ok, waiting_chunks, NewState};
+        false ->
+            {stop, done, NewState}
+    end.
+
+%% @doc Grabs the object for a key and
+%%      checks to see if it's a manifest or
+%%      a regular object. If it's a regular
+%%      object, return it. If it's a manifest,
+%%      start grabbing the chunks and returning them.
+retriever(ReplyPid, Bucket, Key) ->
+    ok.
+
+%% @doc Returns whether or not
+%%      a value is a normal object,
+%%      or a manifest document
+object_or_manifest(Value) ->
+    ok.
+
+%% @doc Remove a chunk from the
+%%      chunks field of State
+remove_chunk(State, Chunk) ->
+    ok.
+
+%% @doc Return true or false
+%%      depending on whether
+%%      we're still waiting
+%%      to accumulate more chunks
+still_waiting(State) ->
