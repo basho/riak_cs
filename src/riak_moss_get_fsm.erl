797978840f820e41caf86f1af7745d49a6587f0e
Reid Draper <reiddraper@gmail.com>
2011-11-21 11:51:03 -0500

    Move shared funcs to a utils module
    AZ940

--- src/riak_moss_get_fsm.erl
+++ src/riak_moss_get_fsm.erl
@@ -45,7 +45,7 @@ prepare(timeout, {_ReplyPid, Bucket, Key}=State) ->
 waiting_value({object, Value}, State) ->
     %% determine if the object is a normal
     %% object, or a manifest object
-    case object_or_manifest(Value) of
+    case riak_moss_lfs_utils:object_or_manifest(Value) of
         object ->
             %% send object back to
             %% the `from` part of
@@ -63,8 +63,8 @@ waiting_chunks({chunk, Chunk}, State) ->
     %% we're assuming that we're receiving the
     %% chunks synchronously, and that we can
     %% send them back to WM as we get them
-    NewState = remove_chunk(State, Chunk),
-    case still_waiting(NewState) of
+    NewState = riak_moss_lfs_utils:remove_chunk(State, Chunk),
+    case riak_moss_lfs_utils:still_waiting(NewState) of
         true ->
             {next_state, waiting_chunks, NewState};
         false ->
@@ -79,24 +79,6 @@ waiting_chunks({chunk, Chunk}, State) ->
 retriever(ReplyPid, Bucket, Key) ->
     gen_fsm:send_event(ReplyPid,
         riak_moss_riakc:get_object(Bucket, Key)).
-
-%% @doc Returns whether or not
-%%      a value is a normal object,
-%%      or a manifest document
-object_or_manifest(_Value) ->
-    ok.
-
-%% @doc Remove a chunk from the
-%%      chunks field of State
-remove_chunk(_State, _Chunk) ->
-    ok.
-
-%% @doc Return true or false
-%%      depending on whether
-%%      we're still waiting
-%%      to accumulate more chunks
-still_waiting(_State) ->
-    ok.
 
 %% @private
 handle_event(_Event, _StateName, StateData) ->
