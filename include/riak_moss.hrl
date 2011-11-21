4f6baf964ff044821aecc02f30204a5bcefb8257
Reid Draper <reiddraper@gmail.com>
2011-11-21 16:10:00 -0500

    More block helper funcs
    AZ944

--- include/riak_moss.hrl
+++ include/riak_moss.hrl
@@ -32,4 +32,5 @@
 
 -define(USER_BUCKET, <<"moss.users">>).
 -define(MAX_CONTENT_LENGTH, 10485760).
+-define(LFS_BLOCK_SIZE, 10485760).
 
--- src/riak_moss_lfs_utils.erl
+++ src/riak_moss_lfs_utils.erl
@@ -11,7 +11,11 @@
 
 -export([is_manifest/1,
          remove_block/2,
-         still_waiting/1]).
+         still_waiting/1,
+         block_count/1,
+         block_name/3,
+         block_name_to_term/1,
+         initial_blocks/1]).
 
 %% @doc Returns true if Value is
 %%      a manifest record
@@ -25,10 +29,36 @@ remove_block(Manifest, Chunk) ->
     Updated = sets:del_element(Chunk, Remaining),
     Manifest#lfs_manifest{blocks_remaining=Updated}.
 
-
 %% @doc Return true or false
 %%      depending on whether
 %%      we're still waiting
 %%      to accumulate more chunks
 still_waiting(#lfs_manifest{blocks_remaining=Remaining}) ->
     sets:size(Remaining) =/= 0.
+
+%% @doc A set of all of the blocks that
+%%      make up the file.
+initial_blocks(ContentLength) ->
+    UpperBound = block_count(ContentLength),
+    Seq = lists:seq(0, (UpperBound - 1)),
+    sets:from_list(Seq).
+
+block_name(Key, UUID, Number) ->
+    term_to_binary({Key, UUID, Number}).
+
+block_name_to_term(Name) ->
+    binary_to_term(Name).
+
+%% @doc The number of blocks that this
+%%      size will be broken up into
+block_count(ContentLength) ->
+    block_count(ContentLength, ?LFS_BLOCK_SIZE).
+
+block_count(ContentLength, BlockSize) ->
+    Quotient = ContentLength div BlockSize,
+    case ContentLength rem BlockSize of
+        0 ->
+            Quotient;
+        _ ->
+            Quotient + 1
+    end.
