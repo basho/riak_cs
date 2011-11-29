4be09983dd178c14db135b8454cf580d0a2f2696
Reid Draper <reiddraper@gmail.com>
2011-11-29 10:57:16 -0500

    Skeleton for riak_moss_lfs_utils test
    AZ944

---
+++ test/riak_moss_lfs_utils_test.erl
@@ -0,0 +1,30 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2007-2011 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% -------------------------------------------------------------------
+
+-module(riak_moss_lfs_utils_test).
+
+-export([lfs_utils_test_/0]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+setup() ->
+    application:start(lager).
+
+%% TODO:
+%% Implement this
+teardown(_) ->
+    ok.
+
+lfs_utils_test_() ->
+    {setup,
+     fun setup/0,
+     fun teardown/1,
+     [
+      fun test/0
+     ]}.
+
+test() ->
+    ?assertEqual(foo, foo).
