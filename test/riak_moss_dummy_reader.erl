60d32a01ca60fccca2e1f18c06351e9e47d0e17c
Reid Draper <reiddraper@gmail.com>
2012-01-19 14:11:21 -0700

    Intermediate checkin

--- test/riak_moss_dummy_reader.erl
+++ test/riak_moss_dummy_reader.erl
@@ -84,7 +84,8 @@ handle_cast({get_manifest, Bucket, Key}, #state{content_length=ContentLength,
     riak_moss_get_fsm:manifest(CallerPid, {ok, RiakObject}),
     {noreply, State};
 handle_cast({get_chunk, Bucket, Key, UUID, ChunkSeq}, #state{caller_pid=CallerPid, block_size=BlockSize}=State) ->
-    FakeData = list_to_binary(["a" || _ <- lists:seq(1, BlockSize)]),
+    BigFakeData = list_to_binary(["a" || _ <- lists:seq(1, BlockSize - 4)]),
+    FakeData = <<ChunkSeq:4/integer, BigFakeData/binary>>,
     RiakObject = riakc_obj:new_obj(Bucket,riak_moss_lfs_utils:block_name(Key, UUID, ChunkSeq), [], [{dict:new(),FakeData}]),
     riak_moss_get_fsm:chunk(CallerPid, ChunkSeq, {ok, RiakObject}),
     {noreply, State};
--- test/riak_moss_get_fsm_eqc.erl
+++ test/riak_moss_get_fsm_eqc.erl
@@ -29,14 +29,18 @@
 -export([prop_get_fsm/0]).
 
 %% States
--export([stopped/2]).
+-export([start/1]).
 
 %% Helpers
 -export([]).
 
 -define(TEST_ITERATIONS, 500).
 
--record(state, {}).
+-record(state, {fsm_pid :: pid(), %% real pid}
+                content_length :: integer(), %% not symbolic
+                total_blocks :: integer(), %% not symbolic
+                last_chunk :: binary(), %% not symbolic
+                counter=0 :: integer()}). %% not symbolic
 
 %% ====================================================================
 %% Public API
@@ -54,6 +58,7 @@ test(Iterations) ->
 
 prop_get_fsm() ->
     ?FORALL(Cmds, commands(?MODULE),
+        #state{content_length=moss_gen:bounded_content_length()},
             begin
                 {H,{_F,S},Res} = run_commands(?MODULE, Cmds),
                 equals(ok, Res)
@@ -67,25 +72,48 @@ prop_get_fsm() ->
 %% Helpers
 %%====================================================================
 
+check_chunk(Counter, Chunk) ->
+    <<Counter:4/integer, _>> = Chunk.
+
 %%====================================================================
 %% eqc_fsm callbacks
 %%====================================================================
 
 initial_state() ->
-    {stopped, true}.
+    {start, true}.
 
-initial_state_data() ->
-    ok.
-
+next_state_data(start, waiting_chunk, #state{content_length=ContentLength}=S, _R, _C) ->
+    BlockSize = riak_moss_lfs_utils:block_size(),
+    {ok, FSMPid} = riak_moss_get_fsm:test_link(<<"bucket">>, <<"key">>, ContentLength, BlockSize),
+    BlockCount = riak_moss_lfs_utils:block_count(ContentLength, BlockSize),
+    _Metadata = riak_moss_get_fsm:get_metadata(FSMPid),
+    riak_moss_get_fsm:continue(FSMPid),
+    S#state{fsm_pid=FSMPid, total_blocks=BlockCount};
+next_state_data(waiting_chunk, waiting_chunk, #state{fsm_pid=FSMPid}=S, _R, _C) ->
+    NextChunk = riak_moss_get_fsm:get_next_chunk(FSMPid),
+    S#state{last_chunk=NextChunk}.
 next_state_data(_From, _To, S, _R, _C) ->
     S.
 
-stopped(_Args, _S) ->
+start(#state{content_length=ContentLength}) ->
+    [{waiting_chunk, {call, ?MODULE, waiting_chunk, []}}].
+
+waiting_chunk(S) ->
+    [{waiting_chunk, {call, ?MODULE, waiting_chunk, []}},
+     {stop, {call, ?MODULE, stop, []}}].
+
+stop(S) ->
     [].
 
+precondition(waiting_chunk,stop,S#{counter=Counter, total_blocks=TotalBlocks,_C) ->
+    Counter == TotalBlocks;
 precondition(_From,_To,_S,_C) ->
     true.
 
+postcondition(waiting_chunk, waiting_chunk, #state{counter=Counter,last_chunk={done, Chunk},total_blocks=Counter, _C, _R) ->
+    check_chunk(Counter, Chunk);
+postcondition(waiting_chunk, waiting_chunk, #state{counter=Counter,last_chunk={chunk, Chunk}, _C, _R) ->
+    check_chunk(Counter, Chunk);
 postcondition(_From, _To, _S, _C, _R) ->
     true.
 
