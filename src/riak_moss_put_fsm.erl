0b2d93262acd986c84f24480d69dccb327bfafd4
argv0 <andy@andygross.org>
2012-01-23 10:50:03 -0800

    better shutdown of writer processes

--- src/riak_moss_put_fsm.erl
+++ src/riak_moss_put_fsm.erl
@@ -165,6 +165,7 @@ initialize(timeout, State=#state{bucket=Bucket,
     %% Start the worker to perform the writing
     case start_writer() of
         {ok, WriterPid} ->
+            link(WriterPid),
             %% Provide the writer with the file details
             riak_moss_writer:initialize(WriterPid,
                                         self(),
@@ -236,13 +237,15 @@ write_block(root_ready, State=#state{data=Data,
             {next_state, write_root, UpdState, Timeout}
     end;
 write_block({all_blocks_written, Manifest}, State=#state{manifest_waiter=Waiter,
-                                                         timeout=Timeout}) ->
+                                                         timeout=Timeout,
+                                                         writer_pid=WriterPid}) ->
     NewState = State#state{final_manifest=Manifest},
     case Waiter of
         undefined ->
             {next_state, client_wait, NewState, Timeout};
         Waiter ->
             gen_fsm:reply(Waiter, {ok, Manifest}),
+            riak_moss_writer:stop(WriterPid),
             {stop, normal, NewState}
     end.
 
@@ -250,10 +253,12 @@ write_block({all_blocks_written, Manifest}, State=#state{manifest_waiter=Waiter,
 %% that has been received by the fsm has been written, but
 %% more data for the file remains.
 -spec waiting(timeout, state()) -> {stop, timeout, state()}.
-waiting(timeout, State) ->
+waiting(timeout, State=#state{writer_pid=WriterPid}) ->
+    riak_moss_writer:stop(WriterPid),
     {stop, timeout, State}.
 
-client_wait(timeout, State) ->
+client_wait(timeout, State=#state{writer_pid=WriterPid}) ->
+    riak_moss_writer:stop(WriterPid),
     {stop, timeout, State}.
 
 %% @doc Handle events that should be handled
@@ -314,7 +319,9 @@ handle_sync_event({augment_data, NewData},
     end;
 handle_sync_event(finalize, From, StateName, State=#state{final_manifest=undefined}) ->
     {next_state, StateName, State#state{manifest_waiter=From}};
-handle_sync_event(finalize, _From, _StateName, State=#state{final_manifest=M}) ->
+handle_sync_event(finalize, _From, _StateName, State=#state{final_manifest=M,
+                                                            writer_pid=WriterPid}) ->
+    riak_moss_writer:stop(WriterPid),
     {stop, normal, {ok, M}, State};
 handle_sync_event(_Event, _From, StateName, State) ->
     {next_state, StateName, State}.
