8e8199e2319602f388a136be60e77128da702e28
Kelly McLaughlin <kelly@basho.com>
2012-02-02 12:56:00 -0700

    Internal storage and handling of acls.
    Fixes #19
    
    * Add module for internal handling of acls.
    * Tests for acl utils module.
    * Convert most functions to use updated user record structure.

--- include/riak_moss.hrl
+++ include/riak_moss.hrl
@@ -1,17 +1,23 @@
-
 -record(moss_user, {
+          name :: string(),
+          key_id :: string(),
+          key_secret :: string(),
+          buckets = []}).
+
+-record(moss_user_v1, {
           name :: string(),
           display_name :: string(),
           email :: string(),
           key_id :: string(),
           key_secret :: string(),
-          canonical_id :: string(),
-          buckets = []}).
--type moss_user() :: #moss_user{}.
+          canonical_id :: string()}).
+-type moss_user() :: #moss_user_v1{}.
 
+-type bucket_acl() :: string().
 -record(moss_bucket, {
           name :: string(),
-          creation_date :: term()}).
+          creation_date :: term(),
+          acl :: bucket_acl()}).
 -type moss_bucket() :: #moss_bucket{}.
 
 -record(context, {auth_bypass :: atom(),
@@ -25,6 +31,16 @@
                       key :: list(),
                       size :: non_neg_integer()}).
 
+-type acl_perm() :: 'READ' | 'WRITE' | 'READ_ACP' | 'WRITE_ACP' | 'FULL_CONTROL'.
+-type acl_perms() :: [acl_perm()].
+-type acl_grant() :: {{string(), string()}, acl_perms()}.
+-record(acl_v1, {owner={"", ""} :: {string(), string()},
+                 grants=[] :: [acl_grant()]}).
+-type acl_v1() :: #acl_v1{}.
+
+-define(ACL, #acl_v1).
+-define(MOSS_USER, #moss_user_v1).
 -define(USER_BUCKET, <<"moss.users">>).
 -define(MAX_CONTENT_LENGTH, 5368709120). %% 5 GB
 -define(DEFAULT_LFS_BLOCK_SIZE, 1048576).%% 1 MB
+-define(XML_PROLOG, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>").
