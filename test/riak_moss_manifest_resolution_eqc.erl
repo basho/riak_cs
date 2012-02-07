0bd612b51e40726d6927ab03cabab93f39ed6575
Reid Draper <reiddraper@gmail.com>
2012-02-07 18:41:16 -0500

    More sophisticated fake manifest generation
    S24

--- test/riak_moss_manifest_resolution_eqc.erl
+++ test/riak_moss_manifest_resolution_eqc.erl
@@ -35,7 +35,8 @@ resolution_commutative() ->
             MapFun = fun(Mani) ->
                 riak_moss_manifest:new(Mani#lfs_manifest_v2.uuid, Mani)
             end,
-            WrappedManifests = lists:map(MapFun, Manifests),
+            Filtered = only_one_active(Manifests),
+            WrappedManifests = lists:map(MapFun, Filtered),
             Resolved = riak_moss_manifest_resolution:resolve(WrappedManifests),
             ReversedResolved = riak_moss_manifest_resolution:resolve(lists:reverse(WrappedManifests)),
             Resolved == ReversedResolved
@@ -48,18 +49,33 @@ resolution_commutative() ->
 raw_manifest() ->
     #lfs_manifest_v2{uuid=moss_gen:bounded_uuid(),
                      bkey={<<"bucket">>, <<"key">>},
-                     state=moss_gen:manifest_state(),
-                     write_blocks_remaining=ordsets:new(),
-                     delete_blocks_remaining=ordsets:new(),
-                     last_block_written_time=erlang:now(),
-                     last_block_deleted_time=erlang:now()}.
+                     state=moss_gen:manifest_state()}.
 
 manifest() ->
-    raw_manifest().
-    %?LET(Manifest, raw_manifest(), Manifests).
+    ?LET(Manifest, raw_manifest(), process_manifest(Manifest)).
+
+process_manifest(Manifest=#lfs_manifest_v2{state=State}) ->
+    case State of
+        writing ->
+            Manifest#lfs_manifest_v2{last_block_written_time=erlang:now(),
+                                     write_blocks_remaining=blocks_set()};
+        active ->
+            %% this clause isn't
+            %% needed but it makes
+            %% things more clear imho
+            Manifest#lfs_manifest_v2{last_block_deleted_time=erlang:now()};
+        pending_delete ->
+            Manifest#lfs_manifest_v2{last_block_deleted_time=erlang:now(),
+                                     delete_blocks_remaining=blocks_set()};
+        deleted ->
+            Manifest#lfs_manifest_v2{last_block_deleted_time=erlang:now()}
+    end.
 
 manifests() ->
     eqc_gen:list(manifest()).
+
+blocks_set() ->
+    ?LET(L, eqc_gen:list(int()), ordsets:from_list(L)).
 
 %%====================================================================
 %% Helpers
@@ -70,3 +86,17 @@ test() ->
 
 test(Iterations) ->
     eqc:quickcheck(eqc:numtests(Iterations, resolution_commutative())).
+
+only_one_active(Manifests) ->
+    {_, FilteredManifests} = lists:foldl(fun only_one_active_helper/2, {not_found, []}, Manifests),
+    FilteredManifests.
+
+only_one_active_helper(#lfs_manifest_v2{state=active}, {found, List}) ->
+    {found, List};
+only_one_active_helper(Manifest, {found, List}) ->
+    {found, [Manifest | List]};
+only_one_active_helper(Manifest=#lfs_manifest_v2{state=active}, {not_found, List}) ->
+    {found, [Manifest | List]};
+only_one_active_helper(Manifest, {not_found, List}) ->
+    {not_found, [Manifest | List]}.
+
