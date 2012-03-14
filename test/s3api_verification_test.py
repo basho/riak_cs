a750e3d51aa4083391330e075c822ea28f0bff1c
Kelly McLaughlin <kelly@basho.com>
2012-03-14 10:33:54 -0600

    Create users inside the test so it will work across test setups without manual interaction.

--- test/s3api_verification_test.py
+++ test/s3api_verification_test.py
@@ -3,16 +3,25 @@
 from boto.exception import S3ResponseError
 from boto.s3.connection import S3Connection, OrdinaryCallingFormat
 from boto.s3.key import Key
+import urllib2, httplib, json
 import unittest, time, uuid
 
-DISPLAY_NAME="kelly"
-CANONICAL_ID="e05c1e14d17aeb65e9445f957e0fc28b892b40caf9ffb4fa5a8b1d454270544c"
-KEY_ID="0FQIMM2WSEDA0GBRBQ9A"
-SECRET_KEY="wJBF8Cg0LkvpFKTg_VaM-odkMYk2PoweqBlC9Q=="
-HOST="127.0.0.1"
-PORT=8080
+def create_user(host, port, name, email):
+    url = '/user'
+    body = "email=%s&name=%s" % (email, name)
+    conn = httplib.HTTPConnection(host, port)
+    conn.request("POST", url, body)
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+    return json.loads(data)
 
 class S3ApiVerificationTest(unittest.TestCase):
+    host="127.0.0.1"
+    port=8080
+
+    user1=None
+    user2=None
 
     SimpleAcl = "<AccessControlPolicy>" + \
                       "<Owner>" + \
@@ -51,26 +60,37 @@ class S3ApiVerificationTest(unittest.TestCase):
                     "</AccessControlList>" + \
                   "</AccessControlPolicy>"
 
-    def make_connection(self, key_id=KEY_ID, secret_key=SECRET_KEY):
-        return S3Connection(key_id, secret_key, is_secure=False,
-                            host=HOST, port=PORT, debug=False,
+
+    def make_connection(self, user):
+        return S3Connection(user['key_id'], user['key_secret'], is_secure=False,
+                            host=self.host, port=self.port, debug=True,
                             calling_format=OrdinaryCallingFormat() )
 
     @classmethod
     def setUpClass(self):
-        # TODO: Create test user so credentials don't have to be updated
+        # Create test user so credentials don't have to be updated
         # for each test setup.
+        # TODO: Once changes are in place so users can be deleted, use
+        # userX@example.me for email addresses and clean up at the end of
+        # the test run.
+        self.user1 = create_user(self.host, self.port, "user1", str(uuid.uuid4()) + "@example.me")
+        self.user2 = create_user(self.host, self.port, "user2", str(uuid.uuid4()) + "@example.me")
         self.bucket_name = str(uuid.uuid4())
         self.key_name = str(uuid.uuid4())
         self.data = file("/dev/random").read(1024)
-        self.default_acl = self.SimpleAcl % (CANONICAL_ID, DISPLAY_NAME, CANONICAL_ID, DISPLAY_NAME, 'FULL_CONTROL')
-        self.pr_acl = self.PublicReadAcl % (CANONICAL_ID, DISPLAY_NAME, CANONICAL_ID, DISPLAY_NAME, 'FULL_CONTROL')
+
+    def defaultAcl(self, user):
+        return self.SimpleAcl % (user['id'], user['display_name'], user['id'], user['display_name'], 'FULL_CONTROL')
+
+    def prAcl(self, user):
+        return self.PublicReadAcl % (user['id'], user['display_name'], user['id'], user['display_name'], 'FULL_CONTROL')
 
     def setUp(self):
-        self.conn = self.make_connection()
+        self.conn = self.make_connection(self.user1)
 
     def test_auth(self):
-        conn = self.make_connection("BAD_ID", "BAD_KEY")
+        bad_user = json.loads('{"email":"baduser@example.me","display_name":"baduser","name":"user1","key_id":"bad_key","key_secret":"bad_secret","id":"bad_canonical_id"}')
+        conn = self.make_connection(bad_user)
         self.assertRaises(S3ResponseError, conn.get_canonical_user_id)
 
     def test_create_bucket(self):
@@ -107,12 +127,12 @@ class S3ApiVerificationTest(unittest.TestCase):
         # self.assertIn(self.bucket_name,
         #               [b.name for b in self.conn.get_all_buckets()])
          bucket = self.conn.get_bucket(self.bucket_name)
-         self.assertEqual(bucket.get_acl().to_xml(), self.default_acl)
+         self.assertEqual(bucket.get_acl().to_xml(), self.defaultAcl(self.user1))
 
     def test_set_bucket_acl(self):
          bucket = self.conn.get_bucket(self.bucket_name)
          bucket.set_canned_acl('public-read')
-         self.assertEqual(bucket.get_acl().to_xml(), self.pr_acl)
+         self.assertEqual(bucket.get_acl().to_xml(), self.prAcl(self.user1))
 
     def test_get_object_acl(self):
          bucket = self.conn.get_bucket(self.bucket_name)
@@ -120,14 +140,14 @@ class S3ApiVerificationTest(unittest.TestCase):
          k.key = self.key_name
          k.set_contents_from_string(self.data)
          self.assertEqual(k.get_contents_as_string(), self.data)
-         self.assertEqual(k.get_acl().to_xml(), self.default_acl)
+         self.assertEqual(k.get_acl().to_xml(), self.defaultAcl(self.user1))
 
     def test_set_object_acl(self):
-         bucket = self.conn.get_bucket(self.bucket_name)
-         k = Key(bucket)
-         k.key = self.key_name
-         k.set_canned_acl('public-read')
-         self.assertEqual(k.get_acl().to_xml(), self.pr_acl)
+        bucket = self.conn.get_bucket(self.bucket_name)
+        k = Key(bucket)
+        k.key = self.key_name
+        k.set_canned_acl('public-read')
+        self.assertEqual(k.get_acl().to_xml(), self.prAcl(self.user1))
 
 if __name__ == "__main__":
     unittest.main()
