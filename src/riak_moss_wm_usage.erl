7e1bbd02d2aa95112ee3ac893a86b3bfa4e547e9
Bryan Fink <bryan@basho.com>
2012-03-30 14:31:15 -0400

    add riak connection to bogus context for auth to use

--- src/riak_moss_wm_usage.erl
+++ src/riak_moss_wm_usage.erl
@@ -203,8 +203,8 @@ generate_etag(RD, #ctx{etag=undefined}=Ctx) ->
 generate_etag(RD, #ctx{etag=Etag}=Ctx) ->
     {Etag, RD, Ctx}.
 
-forbidden(RD, #ctx{auth_bypass=AuthBypass}=Ctx) ->
-    BogusContext = #context{auth_bypass=AuthBypass},
+forbidden(RD, #ctx{auth_bypass=AuthBypass, riak=Riak}=Ctx) ->
+    BogusContext = #context{auth_bypass=AuthBypass, riakc_pid=Riak},
     Next = fun(NewRD, #context{user=User}) ->
                    forbidden(NewRD, Ctx, User)
            end,
