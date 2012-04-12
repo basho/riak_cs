721f69d8255d31540671f47c98ad0ce2675ec6c1
Scott Lystig Fritchie <slfritchie@snookles.com>
2012-04-12 18:42:16 -0500

    Partial work for riak_moss_utils and riak_moss_web.erl

--- src/riak_moss_web.erl
+++ src/riak_moss_web.erl
@@ -17,10 +17,16 @@ dispatch_table() ->
         undefined ->
             AuthBypass = false
     end,
+    StatsProps = stats_props(),
+
     [
      {[], riak_moss_wm_service, [{auth_bypass, AuthBypass}]},
      {["user"], riak_moss_wm_user, []},
      {["usage", '*'], riak_moss_wm_usage, [{auth_bypass, AuthBypass}]},
      {[bucket], riak_moss_wm_bucket, [{auth_bypass, AuthBypass}]},
-     {[bucket, '*'], riak_moss_wm_key, [{auth_bypass, AuthBypass}]}
+     {[bucket, '*'], riak_moss_wm_key, [{auth_bypass, AuthBypass}]},
+     {[proplists:get_value(prefix, StatsProps)], riak_cs_wm_stats, StatsProps}
     ].
+
+stats_props() ->
+    [{prefix, riak_moss_utils:get_env(riak_moss, stats_urlpath, "stats")}].
