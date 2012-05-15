bbbbbfb997160361d1d2a74828942604f1b882f9
Kelly McLaughlin <kelly@basho.com>
2012-05-15 14:35:04 -0600

    Merge riak_moss_gc into riak_cs_gc module.

--- src/riak_cs_gc.erl
+++ src/riak_cs_gc.erl
@@ -4,7 +4,7 @@
 %%
 %% -------------------------------------------------------------------
 
-%% @doc Functions to support garbage collection of files.
+%% @doc Utility module for garbage collection of files.
 
 -module(riak_cs_gc).
 
@@ -15,11 +15,18 @@
 -endif.
 
 %% export Public API
--export([schedule_manifests/2]).
+-export([key_to_slice/1,
+         schedule_manifests/2,
+         slice_from_time/1,
+         slice_from_time_with_leeway/1,
+         slice_to_key/1]).
 
 %%%===================================================================
 %%% Public API
 %%%===================================================================
+
+key_to_slice(<<Slice:64>>) ->
+    Slice.
 
 %% @doc Copy data for a list of manifests to the
 %% `riak-cs-gc' bucket to schedule them for deletion.
@@ -27,10 +34,21 @@
 schedule_manifests(Manifests, RiakPid) ->
     %% Create a set from the list of manifests
     ManifestSet = build_manifest_set(twop_set:new(), Manifests),
+    _ = lager:debug("Manifests scheduled for deletion: ~p", [ManifestSet]),
     %% Write the set to a timestamped key in the `riak-cs-gc' bucket
     Key = term_to_binary(erlang:now()),
     RiakObject = riakc_obj:new(?GC_BUCKET, Key, term_to_binary(ManifestSet)),
     riakc_pb_socket:put(RiakPid, RiakObject).
+
+slice_from_time(Time) ->
+    Secs = timestamp_to_seconds(Time),
+    slice_from_seconds(Secs).
+
+slice_from_time_with_leeway(Time) ->
+    slice_from_time_with_leeway(Time, delete_leeway_slices()).
+
+slice_to_key(Slice) ->
+    <<Slice:64>>.
 
 %%%===================================================================
 %%% Internal functions
@@ -42,6 +60,34 @@ build_manifest_set(Set, []) ->
 build_manifest_set(Set, [HeadManifest | RestManifests]) ->
     UpdSet = twop_set:add_element(HeadManifest, Set),
     build_manifest_set(UpdSet, RestManifests).
+
+delete_leeway_slices() ->
+    case application:get_env(riak_moss, num_leeway_slices) of
+        undefined ->
+            ?DEFAULT_NUM_LEEWAY_SLICES;
+        {ok, NumLeewaySlices} ->
+            NumLeewaySlices
+    end.
+
+
+seconds_per_slice() ->
+    case application:get_env(riak_moss, gc_seconds_per_slice) of
+        undefined ->
+            ?DEFAULT_GC_SECONDS_PER_SLICE;
+        {ok, Seconds} ->
+            Seconds
+    end.
+
+slice_from_seconds(Secs) ->
+    Secs div seconds_per_slice().
+
+slice_from_time_with_leeway(Time, NumLeewaySlices) ->
+    SecondsAddition = NumLeewaySlices * seconds_per_slice(),
+    TimeInSeconds = timestamp_to_seconds(Time),
+    slice_from_seconds(TimeInSeconds + SecondsAddition).
+
+timestamp_to_seconds({MegaSecs, Secs, _MicroSecs}) ->
+    (MegaSecs * (1000 * 1000)) + Secs.
 
 %% ===================================================================
 %% EUnit tests
