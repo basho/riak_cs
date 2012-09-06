4e69f7d22e3948bbe5f9a2807eaa410736fd187e
Kelly McLaughlin <kelly@basho.com>
2012-09-06 11:45:19 -0600

    Check for an outdated config and log a message if it is the case.

--- src/riak_cs_app.erl
+++ src/riak_cs_app.erl
@@ -26,9 +26,23 @@
 -spec start(start_type(), start_args()) -> {ok, pid()} |
                                            {error, term()}.
 start(_Type, _StartArgs) ->
-    riak_cs_sup:start_link().
+    case is_config_valid() of
+        true ->
+            riak_cs_sup:start_link();
+        false ->
+            lager:error("You must update you Riak CS app.config. Please see the release notes for more information on updating you configuration."),
+            {error, bad_config}
+    end.
 
 %% @doc application stop callback for riak_cs.
 -spec stop(term()) -> ok.
 stop(_State) ->
     ok.
+
+is_config_valid() ->
+    get_env_response_to_bool(application:get_env(riak_cs, connection_pools)).
+
+get_env_response_to_bool({ok, _}) ->
+    true;
+get_env_response_to_bool(_) ->
+    false.
