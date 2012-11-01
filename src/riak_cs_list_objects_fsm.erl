671e65870a3d0199f41ff509a14d523dc050095f
Reid Draper <reiddraper@gmail.com>
2012-11-01 18:07:23 -0400

    more fsm pseudocode

--- src/riak_cs_list_objects_fsm.erl
+++ src/riak_cs_list_objects_fsm.erl
@@ -19,6 +19,8 @@
 %% gen_fsm callbacks
 -export([init/1,
          prepare/2,
+         waiting_list_keys/2,
+         waiting_map_reduce/2,
          handle_event/3,
          handle_sync_event/4,
          handle_info/3,
@@ -47,6 +49,30 @@ init(_Args) ->
 prepare(timeout, State) ->
     %% make the list-keys request here
     {ok, waiting_list_keys, State}.
+
+waiting_list_keys(_Event, State) ->
+    %% if we get an error while we're in this state,
+    %% we still have the option to return `HTTP 500'
+    %% to the client, since we haven't written
+    %% any bytes yet
+
+
+    %% depending on whether we're done listing keys
+    %% or not, transition back into `waiting_list_keys'
+    %% or make a m/r request and head into `waiting_map_reduce'.
+    _ = {next_state, waiting_list_keys, State},
+    {next_state, waiting_map_reduce, State}.
+
+waiting_map_reduce(_Event, State) ->
+    %% depending on whether we have enough results yet
+    %% (some of the manifests we requested may be
+    %% marked as delete) we will either have to
+    %% make another map-reduce request.
+
+
+    _ = {next_state, waiting_map_reduce, State},
+    %% reply the results if we do end up being done
+    {stop, normal, State}.
 
 handle_event(_Event, StateName, State) ->
     {next_state, StateName, State}.
