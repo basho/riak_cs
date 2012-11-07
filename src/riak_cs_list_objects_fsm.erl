668107ae117624d918a3763d24947731f6c8b4ec
Reid Draper <reiddraper@gmail.com>
2012-11-07 16:11:23 -0500

    MOre spec and skeleton

--- src/riak_cs_list_objects_fsm.erl
+++ src/riak_cs_list_objects_fsm.erl
@@ -30,6 +30,8 @@
 
 -record(state, {riakc_pid :: pid(),
                 req :: list_object_request(),
+
+                %% list keys ----
                 list_keys_req_id :: undefined | non_neg_integer(),
                 key_buffer=[] :: list(),
                 keys=[] :: list(),
@@ -37,15 +39,32 @@
                 %% `length/1' is linear time
                 num_keys :: undefined | non_neg_integer(),
 
+                %% map reduce ----
                 %% this field will change, it represents
                 %% the current outstanding m/r request
                 map_red_req_id :: undefined | non_neg_integer(),
-                %% maybe we don't need to save the req. ids
                 mr_requests=[] :: [{StartIdx :: non_neg_integer(),
                                     EndIdx :: non_neg_integer()}],
                 object_buffer=[] :: list()}).
 
+%% some useful shared types
+
 -type state() :: #state{}.
+
+-type fsm_state_return() :: {next_state, atom(), state()} |
+                            {stop, term(), state()}.
+
+-type streaming_req_response() :: {ok, ReqID :: non_neg_integer()} |
+                                  {error, term()}.
+
+-type list_keys_event() :: {ReqID :: non_neg_integer(), done} |
+                           {ReqID :: non_neg_integer(), {keys, list()}} |
+                           {ReqID :: non_neg_integer(), {error, term()}}.
+
+-type mapred_event() :: {ReqID :: non_neg_integer(), done} |
+                        {ReqID :: non_neg_integer(),
+                         {mapred, Phase :: non_neg_integer(), list()}} |
+                        {ReqID :: non_neg_integer, {error, term()}}.
 
 %%%===================================================================
 %%% API
@@ -56,22 +75,23 @@
 %%% gen_fsm callbacks
 %%%===================================================================
 
+-spec init(list()) -> {ok, prepare, state(), 0}.
 init([RiakcPid, Request]) ->
     State = #state{riakc_pid=RiakcPid,
                    req=Request},
     {ok, prepare, State, 0}.
 
+-spec prepare(timeout, state()) -> fsm_state_return().
 prepare(timeout, State=#state{riakc_pid=RiakcPid,
                               req=Request}) ->
     handle_streaming_list_keys_call(make_list_keys_request(RiakcPid, Request),
                                    State).
 
+-spec waiting_list_keys(list_keys_event(), state()) -> fsm_state_return().
 waiting_list_keys({ReqID, done}, State=#state{list_keys_req_id=ReqID}) ->
-    NewState = handle_keys_done(State),
-    {next_state, waiting_map_reduce, NewState};
+    handle_keys_done(State);
 waiting_list_keys({ReqID, {keys, Keys}}, State=#state{list_keys_req_id=ReqID}) ->
-    NewState = handle_keys_received(Keys, State),
-    {next_state, waiting_list_keys, NewState};
+    handle_keys_received(Keys, State);
 waiting_list_keys({ReqID, {error, Reason}}, State=#state{list_keys_req_id=ReqID}) ->
     %% if we get an error while we're in this state,
     %% we still have the option to return `HTTP 500'
@@ -79,6 +99,7 @@ waiting_list_keys({ReqID, {error, Reason}}, State=#state{list_keys_req_id=ReqID}
     %% any bytes yet
     {stop, Reason, State}.
 
+-spec waiting_map_reduce(mapred_event(), state()) -> fsm_state_return().
 waiting_map_reduce({ReqID, done}, State=#state{map_red_req_id=ReqID}) ->
     %% depending on the result of this, we'll either
     %% make another m/r request, or be done
@@ -121,6 +142,8 @@ code_change(_OldVsn, StateName, State, _Extra) ->
 %% function to create a list keys request
 %% this could also be a phase-less map-reduce request
 %% with key filters.
+-spec make_list_keys_request(pid(), list_object_request()) ->
+    streaming_req_response().
 make_list_keys_request(RiakcPid, ?LOREQ{name=BucketName}) ->
     %% hardcoded for now
     ServerTimeout = timer:seconds(60),
@@ -130,11 +153,14 @@ make_list_keys_request(RiakcPid, ?LOREQ{name=BucketName}) ->
                                      ServerTimeout,
                                      infinity).
 
+-spec handle_streaming_list_keys_call(streaming_req_response(), state()) ->
+    fsm_state_return().
 handle_streaming_list_keys_call({ok, ReqID}, State) ->
     {next_state, waiting_list_keys, State#state{list_keys_req_id=ReqID}};
 handle_streaming_list_keys_call({error, Reason}, State) ->
     {stop, Reason, State}.
 
+-spec handle_keys_done(state()) -> fsm_state_return().
 handle_keys_done(State=#state{key_buffer=ListofListofKeys,
                               req=?LOREQ{name=BucketName},
                               riakc_pid=RiakcPid}) ->
@@ -145,7 +171,7 @@ handle_keys_done(State=#state{key_buffer=ListofListofKeys,
                            %% free up the space
                            key_buffer=undefined},
     ManifestBucketName = riak_cs_utils:to_bucket_name(objects, BucketName),
-    handle_map_reduce_call(map_reduce_results(RiakcPid,
+    handle_map_reduce_call(make_map_reduce_request(RiakcPid,
                                               ManifestBucketName,
                                               NewState),
                            State).
@@ -154,36 +180,41 @@ handle_keys_received(Keys, State=#state{key_buffer=PrevKeyBuffer}) ->
     %% this is where we might eventually do a 'top-k' keys
     %% kind of thing, like
     %% `lists:sublist(lists:sort([Keys | PrevKeyBuffer]), BufferSize)'
-    State#state{key_buffer=[Keys | PrevKeyBuffer]}.
+    NewState = State#state{key_buffer=[Keys | PrevKeyBuffer]},
+    {next_state, waiting_list_keys, NewState}.
 
 
 %% Map Reduce stuff
 %%--------------------------------------------------------------------
 
 -spec handle_mapred_done(state()) ->
-    {atom(), atom(), state()}.
-handle_mapred_done(_State) ->
+    fsm_state_return().
+handle_mapred_done(_State=#state{object_buffer=ObjectsSoFar,
+                                req=Request,
+                                num_keys=TotalNumKeys}) ->
+    enough_results(ObjectsSoFar, Request, TotalNumKeys),
     could_query_more_mapreduce(foo, bar),
-    more_results_possible(foo, bar).
+    more_results_possible(foo, bar),
+    %% based on the results of `more_results_possible',
+    %% return a new state transition
+    ok.
 
 -spec handle_mapred_results(list(), state()) ->
     {atom(), atom(), state()}.
 handle_mapred_results(_Results, _State) -> ok.
 
--spec map_reduce_results(pid(), binary(), list()) ->
-    {ok, non_neg_integer()} | {error, term()}.
-map_reduce_results(RiakcPid, ManifestBucketName, Keys) ->
+-spec make_map_reduce_request(pid(), binary(), list()) ->
+    streaming_req_response().
+make_map_reduce_request(RiakcPid, ManifestBucketName, Keys) ->
     BKeyTuples = make_bkeys(ManifestBucketName, Keys),
-    make_map_reduce_request(RiakcPid, BKeyTuples).
+    send_map_reduce_request(RiakcPid, BKeyTuples).
 
--spec make_bkeys(binary(), list()) ->
-    list().
+-spec make_bkeys(binary(), list()) -> list().
 make_bkeys(ManifestBucketName, Keys) ->
     [{ManifestBucketName, Key} || Key <- Keys].
 
--spec make_map_reduce_request(pid(), list()) ->
-    {ok, pid()} | {error, term()}.
-make_map_reduce_request(RiakcPid, BKeyTuples) ->
+-spec send_map_reduce_request(pid(), list()) -> streaming_req_response().
+send_map_reduce_request(RiakcPid, BKeyTuples) ->
     %% TODO: change this:
     %% hardcode 60 seconds for now
     Timeout = timer:seconds(60),
@@ -200,9 +231,12 @@ mapred_query() ->
      {reduce, {modfun, riak_cs_utils, reduce_keys_and_manifests},
       undefined, true}].
 
-handle_map_reduce_call({ok, _Results}, _State) ->
-    enough_results(foo, bar, baz),
-    ok.
+-spec handle_map_reduce_call(streaming_req_response(), state()) ->
+    fsm_state_return().
+handle_map_reduce_call({ok, ReqID}, State) ->
+    {next_state, waiting_map_reduce, State#state{map_red_req_id=ReqID}};
+handle_map_reduce_call({error, Reason}, State) ->
+    {stop, Reason, State}.
 
 -spec enough_results(list(), list_object_request(), non_neg_integer()) ->
     boolean().
@@ -221,6 +255,8 @@ could_query_more_mapreduce([], _TotalKeys) ->
 could_query_more_mapreduce(_Requests, _TotalKeys) ->
     ok.
 
+-spec more_results_possible({non_neg_integer(), non_neg_integer()},
+                            non_neg_integer()) -> boolean().
 more_results_possible({_StartIdx, EndIdx}, TotalKeys)
         when EndIdx < TotalKeys ->
     true;
