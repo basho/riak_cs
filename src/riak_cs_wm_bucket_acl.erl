c41a180f9f0991054257bdb4d0a0204c7013364c
Jordan West <jordanrw@gmail.com>
2012-11-14 14:51:23 -0800

    put back support for updating bucket acl
    * reorangize/cleanup some things in the wm_bucket_acl module:
    * HEAD is no longer an allowed method

--- src/riak_cs_wm_bucket_acl.erl
+++ src/riak_cs_wm_bucket_acl.erl
@@ -11,7 +11,6 @@
          allowed_methods/0,
          content_types_accepted/2,
          accept_body/2,
-         delete_resource/2,
          finish_request/2]).
 
 -export([authorize/2]).
@@ -20,6 +19,27 @@
 
 -include("riak_cs.hrl").
 -include_lib("webmachine/include/webmachine.hrl").
+
+
+%% @doc Get the list of methods this resource supports.
+-spec allowed_methods() -> [atom()].
+allowed_methods() ->
+    ['GET', 'PUT'].
+
+-spec content_types_provided() -> [{string(), atom()}].
+content_types_provided() ->
+    [{"application/xml", to_xml}].
+
+-spec content_types_accepted(term(), term()) -> {[{string(), atom()}], term(), term()}.
+content_types_accepted(RD, Ctx) ->
+    dt_entry(<<"content_types_accepted">>),
+    case wrq:get_req_header("content-type", RD) of
+        undefined ->
+            {[{"application/octet-stream", accept_body}], RD, Ctx};
+        CType ->
+            {Media, _Params} = mochiweb_util:parse_header(CType),
+            {[{Media, accept_body}], RD, Ctx}
+    end.
 
 -spec authorize(term(),term()) -> {boolean() | {halt, term()}, term(), term()}.
 authorize(RD, #context{user=User,
@@ -103,31 +123,6 @@ shift_to_owner(RD, Ctx, OwnerId, RiakPid) when RiakPid /= undefined ->
             riak_cs_s3_response:api_error(bucket_owner_unavailable, RD, Ctx)
     end.
 
-%% @doc Get the list of methods this resource supports.
--spec allowed_methods() -> [atom()].
-allowed_methods() ->
-    dt_entry(<<"allowed_methods">>),
-    %% TODO: add POST
-    %% TODO: add back PUT/DELETE
-    ['HEAD', 'GET'].
-
--spec content_types_provided() -> [{string(), atom()}].
-content_types_provided() ->
-    [{"application/xml", to_xml}].
-
-%% @spec content_types_accepted(reqdata(), context()) ->
-%%          {[{ContentType::string(), Acceptor::atom()}],
-%%           reqdata(), context()}
-content_types_accepted(RD, Ctx) ->
-    dt_entry(<<"content_types_accepted">>),
-    case wrq:get_req_header("content-type", RD) of
-        undefined ->
-            {[{"application/octet-stream", accept_body}], RD, Ctx};
-        CType ->
-            {Media, _Params} = mochiweb_util:parse_header(CType),
-            {[{Media, accept_body}], RD, Ctx}
-    end.
-
 -spec to_xml(term(), #context{}) ->
                     {binary() | {'halt', term()}, term(), #context{}}.
 to_xml(RD, Ctx=#context{start_time=StartTime,
@@ -188,29 +183,6 @@ accept_body(RD, Ctx=#context{user=User,
             Code = riak_cs_s3_response:status_code(Reason),
             dt_return(<<"accept_body">>, [Code], [extract_name(User), Bucket]),
             dt_return_bucket(<<"put_acl">>, [Code], [extract_name(User), Bucket]),
-            riak_cs_s3_response:api_error(Reason, RD, Ctx)
-    end.
-
-%% @doc Callback for deleting a bucket.
--spec delete_resource(term(), term()) -> {boolean() | {'halt', term()}, term, #context{}}.
-delete_resource(RD, Ctx=#context{user=User,
-                                 user_object=UserObj,
-                                 bucket=Bucket,
-                                 riakc_pid=RiakPid}) ->
-    dt_entry(<<"delete_resource">>, [], [extract_name(User), Bucket]),
-    dt_entry_bucket(<<"delete">>, [], [extract_name(User), Bucket]),
-    case riak_cs_utils:delete_bucket(User,
-                                       UserObj,
-                                       Bucket,
-                                       RiakPid) of
-        ok ->
-            dt_return(<<"delete_resource">>, [200], [extract_name(User), Bucket]),
-            dt_return_bucket(<<"delete">>, [200], [extract_name(User), Bucket]),
-            {true, RD, Ctx};
-        {error, Reason} ->
-            Code = riak_cs_s3_response:status_code(Reason),
-            dt_return(<<"delete_resource">>, [Code], [extract_name(User), Bucket]),
-            dt_return_bucket(<<"delete">>, [Code], [extract_name(User), Bucket]),
             riak_cs_s3_response:api_error(Reason, RD, Ctx)
     end.
 
