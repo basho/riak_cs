b681cdec620e12a1503f252d64fbe3e7c0ab65b2
Reid Draper <reiddraper@gmail.com>
2013-01-03 14:35:21 -0500

    Expire list objects ETS cache on WM exit too
    Previously, the list objects ETS cache would expire solely
    based on time. The cache key included the pid of the webmachine
    process, so when the process exited (usually from the HTTP connection
    closing), the cache was no longer going to be used. The timer might
    not clean up the key for a while. This commit adds monitoring of
    the specified pid to be another way that the cache key can be cleaned
    up. Furthermore, two two expiration mechanisms clean each other up.
    Put another way, if the timer goes off, the monitor is cancelled. If
    a 'DOWN' message is received, the timer is cancelled. This code
    _should_ handle all cases where a timer/monitor has been cancelled,
    but the message already made it the mailbox.

--- src/riak_cs_list_objects_ets_cache.erl
+++ src/riak_cs_list_objects_ets_cache.erl
@@ -15,7 +15,8 @@
 %% API
 -export([start_link/0,
          lookup/1,
-         maybe_write/2,
+         can_write/3,
+         can_write/4,
          write/2]).
 
 %% gen_server callbacks
@@ -26,7 +27,11 @@
          terminate/2,
          code_change/3]).
 
--record(state, {tid :: ets:tid()}).
+-define(DICTMODULE, dict).
+
+-record(state, {tid :: ets:tid(),
+                monitor_to_timer = ?DICTMODULE:new() :: ?DICTMODULE(),
+                key_to_monitor = ?DICTMODULE:new() :: ?DICTMODULE()}).
 
 -type state() :: #state{}.
 -type cache_lookup_result() :: {true, [binary()]} | false.
@@ -50,22 +55,83 @@ lookup(Key) ->
             false
     end.
 
--spec maybe_write(binary(), term()) -> ok.
-maybe_write(Key, Value) ->
-    handle_should_write(should_write(Value), Key, Value).
+-spec can_write(CacheKey :: binary(),
+                MonitorPid :: pid(),
+                NumKeys :: non_neg_integer()) -> boolean().
+can_write(CacheKey, MonitorPid, NumKeys) ->
+    can_write(?MODULE, CacheKey, MonitorPid, NumKeys).
 
--spec handle_should_write(boolean(), binary(), term()) -> ok.
-handle_should_write(true, Key, Value) ->
-    write(Key, Value);
-handle_should_write(false, _Key, _Value) ->
+-spec can_write(Pid :: pid() | atom(),
+                CacheKey :: binary(),
+                MonitorPid :: pid(),
+                NumKeys :: non_neg_integer()) -> boolean().
+can_write(ServerPid, CacheKey, MonitorPid, NumKeys) ->
+    Message = {can_write, {CacheKey, MonitorPid, NumKeys}},
+    gen_server:call(ServerPid, Message, infinity).
+
+-spec write(binary(), term()) -> ok.
+write(Key, Value) ->
+    try
+        unsafe_write(Key, Value)
+    catch
+        _:Reason ->
+            lager:warning("List objects cache write failed. Reason: ~p", [Reason]),
+            ok
+    end.
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+-spec init(list()) -> {ok, state()}.
+init([]) ->
+    Tid = new_table(),
+    NewState = #state{tid=Tid},
+    {ok, NewState}.
+
+handle_call(get_tid, _From, State=#state{tid=Tid}) ->
+    {reply, Tid, State};
+handle_call({can_write, {CacheKey, MonitorPid, NumKeys}},
+             _From, State) ->
+    Bool = should_write(NumKeys),
+    NewState = case Bool of
+        true ->
+            update_state_with_refs(CacheKey, MonitorPid, State);
+        false ->
+            State
+    end,
+    {reply, Bool, NewState};
+handle_call(Msg, _From, State) ->
+    lager:debug("got unknown message: ~p", [Msg]),
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info({cache_expiry, ExpiredKey}, State) ->
+    NewState = handle_cache_expiry(ExpiredKey, State),
+    {noreply, NewState};
+handle_info({'DOWN', MonitorRef, process, _Pid, _Info}, State) ->
+    NewState = handle_down(MonitorRef, State),
+    {noreply, NewState};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
     ok.
 
--spec should_write(list()) -> boolean().
-should_write(ListOfKeys) ->
-    NumKeys = lists:flatlength(ListOfKeys),
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+-spec should_write(non_neg_integer()) -> boolean().
+should_write(NumKeys) ->
     enough_keys_to_cache(NumKeys) andalso space_in_cache(NumKeys).
 
--spec enough_keys_to_cache(list()) -> boolean().
+-spec enough_keys_to_cache(non_neg_integer()) -> boolean().
 enough_keys_to_cache(NumKeys) when NumKeys < ?MIN_KEYS_TO_CACHE ->
     false;
 enough_keys_to_cache(_NumKeys) ->
@@ -99,57 +165,73 @@ space_available(_BytesUsed, _ProspectiveAdd, _MaxCacheSize) ->
 num_keys_to_bytes(NumKeys) ->
     NumKeys * 64.
 
--spec write(binary(), term()) -> ok.
-write(Key, Value) ->
-    try
-        unsafe_write(Key, Value)
-    catch
-        _:Reason ->
-            lager:warning("List objects cache write failed. Reason: ~p", [Reason]),
-            ok
-    end.
 
 unsafe_write(Key, Value) ->
     TS = riak_cs_utils:timestamp(os:timestamp()),
     lager:debug("Writing entry for ~p to LO Cache", [Key]),
     ets:insert(?LIST_OBJECTS_CACHE, {Key, Value, TS}),
-    erlang:send_after(?CACHE_TIMEOUT, ?MODULE, {cache_expiry, Key}),
     ok.
 
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
+-spec update_state_with_refs(binary(), pid(), state()) -> state().
+update_state_with_refs(CacheKey, MonitorPid, State) ->
+    MonitorRef = erlang:monitor(process, MonitorPid),
+    TimerRef = erlang:send_after(?CACHE_TIMEOUT, ?MODULE,
+                                 {cache_expiry, CacheKey}),
+    update_state_with_refs_helper(MonitorRef, TimerRef, CacheKey, State).
 
--spec init(list()) -> {ok, state()} | {error, term()}.
-init([]) ->
-    Tid = new_table(),
-    NewState = #state{tid=Tid},
-    {ok, NewState}.
+-spec update_state_with_refs_helper(reference(), reference(), binary(), state()) ->
+    state().
+update_state_with_refs_helper(MonitorRef, TimerRef, CacheKey,
+                              State=#state{monitor_to_timer=MonToTimer,
+                                           key_to_monitor=KeyToMon}) ->
+    NewMonToTimer = ?DICTMODULE:store(MonitorRef, {TimerRef, CacheKey}, MonToTimer),
+    NewKeyToMon = ?DICTMODULE:store(TimerRef, MonitorRef, KeyToMon),
+    State#state{monitor_to_timer=NewMonToTimer,
+                key_to_monitor=NewKeyToMon}.
 
-handle_call(get_tid, _From, State=#state{tid=Tid}) ->
-    {reply, Tid, State};
-handle_call(Msg, _From, State) ->
-    lager:debug("got unknown message: ~p", [Msg]),
-    {reply, ok, State}.
-
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info({cache_expiry, ExpiredKey}, State) ->
+-spec handle_cache_expiry(binary(), state()) -> state().
+handle_cache_expiry(ExpiredKey, State=#state{key_to_monitor=KeyToMon}) ->
     ets:delete(?LIST_OBJECTS_CACHE, ExpiredKey),
-    {noreply, State};
-handle_info(_Info, State) ->
-    {noreply, State}.
+    NewKeyToMon = remove_monitor(ExpiredKey, KeyToMon),
+    State#state{key_to_monitor=NewKeyToMon}.
 
-terminate(_Reason, _State) ->
-    ok.
+-spec handle_down(reference(), state()) -> state().
+handle_down(MonitorRef, State=#state{monitor_to_timer=MonToTimer}) ->
+    NewMonToTimer = remove_timer(MonitorRef, MonToTimer),
+    State#state{monitor_to_timer=NewMonToTimer}.
 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+-spec remove_monitor(binary(), ?DICTMODULE()) -> ?DICTMODULE().
+remove_monitor(ExpiredKey, KeyToMon) ->
+    RefResult = safe_fetch(ExpiredKey, KeyToMon),
+    case RefResult of
+        {ok, Ref} ->
+            true = erlang:demonitor(Ref);
+        {error, _Reason} ->
+            true
+    end,
+    ?DICTMODULE:erase(ExpiredKey, KeyToMon).
 
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
+-spec remove_timer(reference(), ?DICTMODULE()) -> ?DICTMODULE().
+remove_timer(MonitorRef, MonToTimer) ->
+    RefResult = safe_fetch(MonitorRef, MonToTimer),
+    _ = case RefResult of
+        {ok, {TimerRef, CacheKey}} ->
+            %% can be true | false
+            _ = ets:delete(?LIST_OBJECTS_CACHE, CacheKey),
+            erlang:cancel_timer(TimerRef);
+        {error, _Reason} ->
+            true
+    end,
+    ?DICTMODULE:erase(MonitorRef, MonToTimer).
+
+-spec safe_fetch(Key :: term(), Dict :: ?DICTMODULE()) ->
+    {ok, term()} | {error, term()}.
+safe_fetch(Key, Dict) ->
+    try
+        {ok, ?DICTMODULE:fetch(Key, Dict)}
+    catch error:Reason ->
+            {error, Reason}
+    end.
 
 -spec new_table() -> ets:tid().
 new_table() ->
