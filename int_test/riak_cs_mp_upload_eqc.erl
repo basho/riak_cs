abf5b35bb380a578029aecacd9c45bade776954d
Reid Draper <reiddraper@gmail.com>
2013-01-18 10:26:07 -0700

    More work toward getting test to run
    The current way to get the test running is something like:
    
    ```
    cd int_test/
    erl -pa ../ebin ../deps/*/ebin ../rel/riak
    ```
    
    now in the shell
    
    ```
    c(riak_cs_mp_upload_eqc).
    riak_cs_mp_upload_eqc:start_deps().
    riak_cs_mp_upload_eqc:test().
    %% now see the failing test
    ```
    
    The test fails with a function clause error in
    `riak_cs_put_fsm:done`

--- int_test/riak_cs_mp_upload_eqc.erl
+++ int_test/riak_cs_mp_upload_eqc.erl
@@ -4,6 +4,7 @@
 %%
 %% -------------------------------------------------------------------
 
+
 -module(riak_cs_mp_upload_eqc).
 
 -include_lib("eqc/include/eqc.hrl").
@@ -12,9 +13,12 @@
 -compile(export_all).
 -compile({parse_transform, eqc_group_commands}).
 
+-define(NUMTESTS, 100).
 -define(DICTMODULE, orddict).
 
+%% -------------------------------------------------------------------
 %% Constants
+%% -------------------------------------------------------------------
 
 -define(BUCKET, <<"riak_cs_mp_upload_eqc">>).
 -define(KEY, <<"test">>).
@@ -23,7 +27,9 @@
                "329c332b9eff49899c2c6e5185de7fd97cdc6d646c55d3e84ef19006c746c368",
                "LAHU4GBJIRQD55BJNET7"}).
 
+%% -------------------------------------------------------------------
 %% Types
+%% -------------------------------------------------------------------
 
 -type upload_id()   :: string().
 -type upload_ids()  :: list(upload_id()).
@@ -36,53 +42,138 @@
 -type complete()    :: {upload_id(), parts()}.
 -type completes()   :: list(complete()).
 -type aborts()      :: list(upload_id()).
--type pids()        :: list(pid()).
+%%-type pids()        :: list(pid()).
 
 -record(state, {initiated   :: upload_ids(),
                 uploaded    :: uploaded(),
                 completed   :: completes(),
-                aborted     :: aborts(),
-                pids        :: pids()}).
+                aborted     :: aborts()}).
+
+%% -------------------------------------------------------------------
+%% Statem Callbacks
+%% -------------------------------------------------------------------
 
 initial_state() ->
-    {ok, Pid} = riak_cs_utils:riak_connection(),
     #state{initiated    = [],
            uploaded     = ?DICTMODULE:new(),
            completed    = [],
-           aborted      = [],
-           pids         = [Pid]}.
+           aborted      = []}.
 
-next_state(S=#state{initiated=Initiated}, {ok, UploadID},
-           {call, riak_cs_mp_utils, initiate_multipart_upload, _Args}) ->
-    S#state{initiated=[UploadID | Initiated]}.
+next_state(S=#state{initiated=Initiated}, UploadID,
+           {call, ?MODULE, initiate_multipart_upload_wrapper, _Args}) ->
+    S#state{initiated=[UploadID | Initiated]};
+next_state(S=#state{uploaded=Uploaded}, MD5,
+           {call, ?MODULE, upload_part_wrapper, [UploadID, PartNumber, Blob]}) ->
+    NewUploaded = add_part_to_dict(Uploaded, UploadID, {PartNumber, MD5, Blob}),
+    S#state{uploaded=NewUploaded}.
 
 command(S) ->
-    %% * initiate
-    %% * upload part
+    %% * initiate [x]
+    %% * upload part [x]
     %% * complete
     %% * abort
     %% * read
-    oneof([initiate(S)]).
+
+    oneof([initiate(S)] ++ [upload_part(S) || [] =/= S#state.initiated]).
+
+%% -------------------------------------------------------------------
+%% Initiate
 
 %% commands broken out for ease of reading
-initiate(#state{pids=Pids}) ->
-    {call, riak_cs_mp_utils, initiate_multipart_upload,
-     [?BUCKET, ?KEY, ?CTYPE, ?USER, [], elements(Pids)]}.
+initiate(_S) ->
+    {call, ?MODULE, initiate_multipart_upload_wrapper, [{var, pids}, nat()]}.
 
-preconditions(_S, _Call, _Result) ->
+initiate_multipart_upload_wrapper(Pids, Nat) ->
+    PidIndex = (Nat * length(Pids)) rem length(Pids),
+    Pid = lists:nth(PidIndex + 1, Pids),
+    {ok, Id} = riak_cs_mp_utils:initiate_multipart_upload(?BUCKET, ?KEY, ?CTYPE, ?USER, [], Pid),
+    Id.
+
+%% -------------------------------------------------------------------
+%% Upload Part
+
+upload_part(#state{initiated=UploadIDs}) ->
+    %% need to somehow filter out the upload
+    %% ids that are not in the aborted state,
+    %% at least at first
+    UploadID = elements(UploadIDs),
+    Value = binary(),
+    PartID = nat(),
+    {call, ?MODULE, upload_part_wrapper, [UploadID, PartID, Value]}.
+
+upload_part_wrapper(UploadId, PartNumber, Blob) ->
+    Size = byte_size(Blob),
+    {upload_part_ready, _PartUUID, PutPid} =
+        riak_cs_mp_utils:upload_part(?BUCKET, ?KEY, UploadId, PartNumber, Size, ?USER),
+    {ok, MD5} = riak_cs_mp_utils:upload_part_1blob(PutPid, Blob),
+    MD5.
+
+%% -------------------------------------------------------------------
+%% (Pre|Post)conditions
+
+precondition(_S, _Call) ->
     true.
 
 postcondition(_S, _Call, _Result) ->
     true.
 
+%% -------------------------------------------------------------------
+%% Properties
+%% -------------------------------------------------------------------
+
 prop_mp_upload() ->
     ?FORALL(Cmds, commands(?MODULE),
             begin
-            {_H, _S, Res} = run_commands(?MODULE, Cmds),
-            Res==ok
+            %%{ok, Pid} = riak_cs_utils:riak_connection(),
+            %%ok = application:start(poolboy),
+            %%ok = application:start(riak_cs),
+            {ok, Pid} = riakc_pb_socket:start_link("localhost", 8087, []),
+            R={_H, _S, Res} = run_commands(?MODULE, Cmds, [{pids, [Pid]}]),
+            pretty_commands(?MODULE, Cmds, R, Res==ok)
             end).
 
+%% -------------------------------------------------------------------
+%% Test Helpers
+
+test() ->
+    test(?NUMTESTS).
+
+test(NumTests) ->
+    quickcheck(numtests(NumTests,
+                        prop_mp_upload())).
+
+%% -------------------------------------------------------------------
+%% Test Start Helpers
+
+start_deps() ->
+    application:start(inets),
+    application:start(folsom),
+    application:start(crypto),
+    application:start(mochiweb),
+    application:start(webmachine),
+    application:start(lager),
+    application:start(poolboy),
+    application:set_env(riak_cs, connection_pools, [{request_pool, {5, 0}}]),
+    application:start(riak_cs).
+
+stop_deps() ->
+    application:stop(riak_cs),
+    application:unset_env(riak_cs, connection_pools),
+    application:stop(poolboy),
+    application:stop(lager),
+    application:stop(webmachine),
+    application:stop(mochiweb),
+    application:stop(crypto),
+    application:stop(folsom),
+    application:stop(inets).
+
+%% -------------------------------------------------------------------
+%% Generators
+%% -------------------------------------------------------------------
+
+%% -------------------------------------------------------------------
 %% Helpers
+%% -------------------------------------------------------------------
 
 add_part_to_dict(Dict, Key, Part) ->
     Fun = fun(List) -> [Part | List] end,
