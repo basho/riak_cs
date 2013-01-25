305617ec748db25454a4f9ff65dc2e2908ca353a
Kelly McLaughlin <kelly@basho.com>
2013-01-25 16:32:02 -0700

    Support prefix and delimiter options for list objects requets
    Fixes: #378
    
    * Check for prefix and delimiter options in bucket listing requests
    * Add common prefix plumbing to list objects fsm
    * Produce CommonPrefixes xml in list objects response documents
    * Account for CommonPrefixes when adjusting list objects response for
      max-keys specification
    * Update list_object_test to test max-keys, marker, prefix, and
      delimiter request options

--- riak_test/tests/list_objects_test.erl
+++ riak_test/tests/list_objects_test.erl
@@ -32,7 +32,7 @@ confirm() ->
     Count1 = 10,
     load_objects(?TEST_BUCKET, Count1, UserConfig),
 
-    %%Successively list the buckets, verify the output, and delete an
+    %% Successively list the buckets, verify the output, and delete an
     %% object from the bucket until the bucket is empty again.
     delete_and_verify_objects(?TEST_BUCKET, Count1, UserConfig),
 
@@ -40,9 +40,42 @@ confirm() ->
     Count2 = 100,
     load_objects(?TEST_BUCKET, Count2, UserConfig),
 
-    %%Successively list the buckets, verify the output, and delete an
+    %% Successively list the buckets, verify the output, and delete an
     %% object from the bucket until the bucket is empty again.
     delete_and_verify_objects(?TEST_BUCKET, Count2, UserConfig),
+
+    %% Put 30 objects in the bucket
+    Count3 = 30,
+    load_objects(?TEST_BUCKET, Count3, UserConfig),
+
+    %% Use `max-keys' to restrict object list to 20 results
+    Options1 = [{max_keys, 20}],
+    ObjList1 = erlcloud_s3:list_objects(?TEST_BUCKET, Options1, UserConfig),
+    Marker1 = proplists:get_value(marker, ObjList1),
+    verify_object_list(ObjList1, 20),
+
+    %% Use `marker' to request remainder of results
+    Options2 = [{marker, Marker1}],
+    verify_object_list(erlcloud_s3:list_objects(?TEST_BUCKET, Options2, UserConfig), 10),
+
+    %% Put 2 sets of 4 objects with keys that have a common subdirectory
+    load_objects(?TEST_BUCKET, 4, "subdir/prefix1/", UserConfig),
+    load_objects(?TEST_BUCKET, 4, "subdir/prefix2/", UserConfig),
+
+    %% Use `max-keys', `prefix' and `delimiter' to get first 30
+    %% results back and verify results are truncated and 2 common
+    %% prefixes are returned.
+    %% Unable to currently verify the `CommonPrefixes' for this is
+    %% fact that ercloud does not support parsing the those response
+    %% elements.
+    Options3 = [{max_keys, 30}, {prefix, "subdir"}, {delimiter, "/"}],
+    ObjList2 = erlcloud_s3:list_objects(?TEST_BUCKET, Options3, UserConfig),
+    Marker2 = proplists:get_value(marker, ObjList2),
+    verify_object_list(ObjList1, 28),
+
+    %% Request remainder of results
+    Options3 = [{marker, Marker2}],
+    verify_object_list(erlcloud_s3:list_objects(?TEST_BUCKET, Options3, UserConfig), 2),
 
     lager:info("deleting bucket ~p", [?TEST_BUCKET]),
     ?assertEqual(ok, erlcloud_s3:delete_bucket(?TEST_BUCKET, UserConfig)),
@@ -51,8 +84,11 @@ confirm() ->
     pass.
 
 load_objects(Bucket, Count, Config) ->
+    load_objects(Bucket, Count, [], Config).
+
+load_objects(Bucket, Count, KeyPrefix, Config) ->
     [erlcloud_s3:put_object(Bucket,
-                            integer_to_list(X),
+                            KeyPrefix ++ integer_to_list(X),
                             crypto:rand_bytes(1000),
                             Config) || X <- lists:seq(1,Count)].
 
