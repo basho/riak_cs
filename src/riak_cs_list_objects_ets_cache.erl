13da832df41cbc6f5a7f19d976b6f44456883f72
Kelly McLaughlin <kelly@basho.com>
2013-02-07 16:09:59 -0700

    Address dialyzer complaints about lager return matching

--- src/riak_cs_list_objects_ets_cache.erl
+++ src/riak_cs_list_objects_ets_cache.erl
@@ -56,11 +56,11 @@ start_link() ->
 -spec lookup(binary()) -> cache_lookup_result().
 lookup(Key) ->
     try
-        lager:debug("Reading info for ~p from cache", [Key]),
+        _ = lager:debug("Reading info for ~p from cache", [Key]),
         format_lookup_result(ets:lookup(default_ets_table(), Key))
     catch
         _:Reason ->
-            lager:warning("List objects cache lookup failed. Reason: ~p", [Reason]),
+            _ = lager:warning("List objects cache lookup failed. Reason: ~p", [Reason]),
             false
     end.
 
@@ -84,7 +84,7 @@ write(Key, Value) ->
         unsafe_write(Key, Value)
     catch
         _:Reason ->
-            lager:warning("List objects cache write failed. Reason: ~p", [Reason]),
+            _ = lager:warning("List objects cache write failed. Reason: ~p", [Reason]),
             ok
     end.
 
@@ -120,7 +120,7 @@ handle_call({can_write, {CacheKey, MonitorPid, NumKeys}},
     end,
     {reply, Bool, NewState};
 handle_call(Msg, _From, State) ->
-    lager:debug("got unknown message: ~p", [Msg]),
+    _ = lager:debug("got unknown message: ~p", [Msg]),
     {reply, ok, State}.
 
 handle_cast(_Msg, State) ->
@@ -211,7 +211,7 @@ num_keys_to_bytes(NumKeys) ->
 
 unsafe_write(Key, Value) ->
     TS = riak_cs_utils:timestamp(os:timestamp()),
-    lager:debug("Writing entry for ~p to LO Cache", [Key]),
+    _ = lager:debug("Writing entry for ~p to LO Cache", [Key]),
     ets:insert(default_ets_table(), {Key, Value, TS}),
     ok.
 
