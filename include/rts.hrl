a3e1578a31b3c948ea604b792d7a3cfe11b38e1f
UENISHI Kota <kuenishi@gmail.com>
2013-02-12 18:11:55 +0900

    add records not in atom table

--- include/rts.hrl
+++ include/rts.hrl
@@ -1,3 +1,6 @@
 %% JSON keys used by rts module
 -define(START_TIME, <<"StartTime">>).
 -define(END_TIME, <<"EndTime">>).
+
+-define(START_TIME_ATOM, 'StartTime').
+-define(END_TIME_ATOM, 'EndTime').
