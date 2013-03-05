768291e97a029a451ec68caa5d214803535e5f9a
Scott Lystig Fritchie <slfritchie@snookles.com>
2013-03-04 19:15:59 -0600

    riak_test automation for the Python client tests.

---
+++ riak_test/tests/external_client_tests.erl
@@ -0,0 +1,53 @@
+-module(external_client_tests).
+
+-export([confirm/0]).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(TEST_BUCKET, "riak_test_bucket").
+
+confirm() ->
+    CS_extra = [{enforce_multipart_part_size, false}],
+    {RiakNodes, _CSNodes, _Stanchion} =
+        rtcs:setup(1,
+                   rtcs:ee_config(),
+                   rtcs:stanchion_config(),
+                   rtcs:cs_config(CS_extra)),
+
+    FirstNode = hd(RiakNodes),
+
+    {AccessKeyId, SecretAccessKey} = rtcs:create_user(FirstNode, 1),
+
+    %% User config
+    CS_http_port = rtcs:cs_port(FirstNode),
+    _UserConfig = rtcs:config(AccessKeyId, SecretAccessKey, CS_http_port),
+
+    lager:debug("cs_src_root = ~p", [rtdev:relpath(cs_src_root)]),
+    %% NOTE: This 'riak_cs_root' path must appear in
+    %% ~/.riak_test.config in the 'rtdev' section, 'rtdev_path'
+    %% subsection.
+    CS_src_dir = rtdev:relpath(cs_src_root),
+    StdoutStderr = "/tmp/my_output." ++ os:getpid(),
+    os:cmd("rm -f " ++ StdoutStderr),
+
+    Cmd = lists:flatten(
+            io_lib:format("cd ~s; env CS_HTTP_PORT=~p make test-boto > ~s 2>&1 ; echo Res $?",
+                          [CS_src_dir, CS_http_port, StdoutStderr])),
+    lager:debug("Cmd = ~s", [Cmd]),
+    Res = os:cmd(Cmd),
+    lager:debug("YYY res = ~s", [Res]),
+
+    try
+        case Res of
+            "Res 0\n" ->
+                pass;
+            _ ->
+                lager:error("Expected 0, got: ~s", [Res]),
+                {ok, DebugOut0} = file:read_file(StdoutStderr),
+                DebugOut = re:replace(DebugOut0, "\n", "\\\\n",
+                                      [global, multiline, {return, list}]),
+                lager:error("Script output: ~s", [DebugOut]),
+                error(fail)
+        end
+    after
+        os:cmd("rm -f " ++ StdoutStderr)
+    end.
