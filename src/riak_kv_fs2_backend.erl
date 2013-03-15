213aeee94f2cd3e0cf3fef53c37bf52bbd4abe73
Andy Gross <andy@andygross.org>
2013-03-15 14:31:53 -0700

    Join us now and share the software (Add Apache license headers).

--- src/riak_kv_fs2_backend.erl
+++ src/riak_kv_fs2_backend.erl
@@ -2,7 +2,7 @@
 %%
 %% riak_fs2_backend: storage engine based on basic filesystem access
 %%
-%% Copyright (c) 2007-2012 Basho Technologies, Inc.  All Rights Reserved.
+%% Copyright (c) 2007-2013 Basho Technologies, Inc.  All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
