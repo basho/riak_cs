9c71ef1ae4b185226145b456aaef9463bf32b5a3
Reid Draper <reiddraper@gmail.com>
2013-04-10 15:13:12 -0400

    Hook it up with webmachine

--- src/riak_cs_list_objects_fsm_v2.erl
+++ src/riak_cs_list_objects_fsm_v2.erl
@@ -30,14 +30,24 @@ list_objects(RiakcPid, Request) ->
     RiakObjects = make_2i_request(RiakcPid, Request),
     Manifests = [riak_cs_utils:manifests_from_riak_object(O) ||
                  O <- RiakObjects],
-    map_active_manifests(Manifests).
+    Active = map_active_manifests(Manifests),
+    Filtered = exclude_marker(Request, Active),
+    KeyContent = lists:map(fun riak_cs_list_objects:manifest_to_keycontent/1,
+                           Filtered),
+    case KeyContent of
+        [] ->
+            riak_cs_list_objects:new_response(Request, false, [], []);
+        _Else ->
+            riak_cs_list_objects:new_response(Request, true, [], KeyContent)
+    end.
 
 -spec make_2i_request(pid(), list_object_request()) -> [riakc_obj:riakc_obj()].
-make_2i_request(RiakcPid, Request=?LOREQ{name=BucketName}) ->
+make_2i_request(RiakcPid, Request=?LOREQ{name=BucketName,
+                                         max_keys=MaxKeys}) ->
     ManifestBucket = riak_cs_utils:to_bucket_name(objects, BucketName),
     StartKey = make_start_key(Request),
     EndKey = big_end_key(128),
-    Opts = [{return_terms, true}, {max_results, 1000}, {stream, true}],
+    Opts = [{return_terms, true}, {max_results, MaxKeys}, {stream, true}],
     {ok, ReqID} = riakc_pb_socket:get_index_range(RiakcPid,
                                                   ManifestBucket,
                                                   <<"$key">>,
@@ -78,3 +88,16 @@ map_active_manifests(Manifests) ->
     ActiveTuples = [riak_cs_manifest_utils:active_manifest(M) ||
                     M <- Manifests],
     [A || {ok, A} <- ActiveTuples].
+
+-spec exclude_marker(list_object_request(), list()) -> list().
+exclude_marker(?LOREQ{marker=undefined}, Objects) ->
+    Objects;
+exclude_marker(_Request, []) ->
+    [];
+exclude_marker(?LOREQ{marker=Marker}, [H | T]=Objects) ->
+    case element(2, H?MANIFEST.bkey) == Marker of
+        true ->
+            T;
+        false ->
+            Objects
+    end.
