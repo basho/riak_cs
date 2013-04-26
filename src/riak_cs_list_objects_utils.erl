4c674b8df7689cc816e1ea36b9140092e2944054
Reid Draper <reiddraper@gmail.com>
2013-04-26 14:59:22 -0400

    Add an abstraction between old and new list keys
    The API is the same once you've called `start_link`. So this code has
    you pass in the greater or the args between them (the old one), and then
    returns a pid using either the old or the new one, dependent on the
    value of `fold_objects_for_list_keys`.

---
+++ src/riak_cs_list_objects_utils.erl
@@ -0,0 +1,62 @@
+%% ---------------------------------------------------------------------
+%%
+%% Copyright (c) 2007-2013 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% ---------------------------------------------------------------------
+
+%% @doc
+
+-module(riak_cs_list_objects_utils).
+
+-include("riak_cs.hrl").
+-include("list_objects.hrl").
+
+%% API
+-export([start_link/5]).
+
+%% Configuration
+-export([fold_objects_for_list_keys/0]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+
+-spec start_link(pid(), pid(), list_object_request(), term(), UseCache :: boolean()) ->
+    {ok, pid()} | {error, term()}.
+%% @doc An abstraction between the old and new list-keys mechanism. Uses the
+%% old mechanism if `fold_objects_for_list_keys' is false, otherwise uses
+%% the new one. After getting a pid back, the API is the same, so users don't
+%% need to differentiate.
+start_link(RiakcPid, CallerPid, ListKeysRequest, CacheKey, UseCache) ->
+    case fold_objects_for_list_keys() of
+        true ->
+            riak_cs_list_objects_fsm_v2:start_link(RiakcPid, ListKeysRequest);
+        false ->
+            riak_cs_list_objects_fsm:start_link(RiakcPid, CallerPid,
+                                                ListKeysRequest, CacheKey,
+                                                UseCache)
+    end.
+
+%%%===================================================================
+%%% Configuration
+%%%===================================================================
+
+-spec fold_objects_for_list_keys() -> boolean().
+fold_objects_for_list_keys() ->
+    riak_cs_utils:get_env(riak_cs, fold_objects_for_list_keys,
+                          ?FOLD_OBJECTS_FOR_LIST_KEYS).
