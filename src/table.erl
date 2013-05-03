896576207d064764f719825b6fe2059dca7befa8
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2013-05-03 14:22:37 -0400

    working tables and one for manifests

--- src/table.erl
+++ src/table.erl
@@ -16,8 +16,8 @@ create_table(Spec, Rows) ->
 
 -spec create_table(list(), list(), non_neg_integer(), iolist()) -> iolist().
 create_table(Spec, Rows, Length, []) ->
-    FirstTwoRows = [titles(Spec), vertical_border(Length)],
-    create_table(Spec, Rows, Length, FirstTwoRows);
+    FirstThreeRows = [vertical_border(Length), titles(Spec), vertical_border(Length)],
+    create_table(Spec, Rows, Length, FirstThreeRows);
 create_table(_Spec, [], Length, IoList) when length(IoList) =:= 2 ->
     BottomBorder = vertical_border(Length),
     %% There are no more rows to print so return the table
@@ -25,18 +25,20 @@ create_table(_Spec, [], Length, IoList) when length(IoList) =:= 2 ->
 create_table(_Spec, [], _Length, IoList) ->
     lists:reverse(IoList);
 create_table(Spec, [Row | Rows], Length, IoList) ->
-    Zipped = lists:zip(Spec, Row),
-    create_table(Spec, Rows, Length, [row(Zipped) | IoList]).
+    create_table(Spec, Rows, Length, [row(Spec, Row) | IoList]).
 
 -spec get_row_length(list(tuple())) -> non_neg_integer().
 get_row_length(Spec) ->
     lists:foldl(fun({_Name, Size}, Total) ->
                     Total + Size + 2 
-                end, 0, Spec).
+                end, 0, Spec) + 2.
 
--spec row(list({list(), list()})) -> iolist().
-row(_) ->
-    "".
+-spec row(list(), list(string())) -> iolist().
+row(Spec, Row) ->
+    ["| " | lists:reverse(
+        ["\n" | lists:foldl(fun({{_, Size}, Str}, Acc) ->
+                                [align(Str, Size) | Acc]
+                            end, [], lists:zip(Spec, Row))])].
 
 -spec titles(list()) -> iolist().
 titles(Spec) ->
@@ -46,17 +48,22 @@ titles(Spec) ->
                             end, [], Spec)])].
 
 -spec align(string(), non_neg_integer()) -> iolist().
+align(Str, Size) when is_integer(Str) ->
+    align(integer_to_list(Str), Size);
+align(Str, Size) when is_binary(Str) ->
+    align(binary_to_list(Str), Size);
+align(Str, Size) when is_atom(Str) ->
+    align(atom_to_list(Str), Size);
 align(Str, Size) when length(Str) > Size -> 
     Truncated = lists:sublist(Str, Size),
     Truncated ++ " |"; 
 align(Str, Size) when length(Str) =:= Size ->
     Str ++ " |";
-align(Str, Size) when (((Size - length(Str))/2) rem 2) =:= 0 ->
-    Spaces = (Size - length(Str)) div 2,
-    spaces(Spaces) ++ Str ++ spaces(Spaces) ++ " |";
 align(Str, Size) ->
-    Spaces = (Size - length(Str)) div 2,
-    spaces(Spaces) ++ Str ++ spaces(Spaces) ++ " |".
+    StrLen = length(Str),
+    LeftSpaces = (Size - StrLen) div 2,
+    RightSpaces = Size - StrLen - LeftSpaces,
+    spaces(LeftSpaces) ++ Str ++ spaces(RightSpaces) ++ " |".
 
 -spec vertical_border(non_neg_integer()) -> string().
 vertical_border(Length) ->
