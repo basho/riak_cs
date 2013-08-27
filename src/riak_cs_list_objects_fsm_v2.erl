82fc60b75e1afbb4f2feb5ec3cec0c0461a35790
Reid Draper <reiddraper@gmail.com>
2013-08-27 10:55:53 -0500

    Never rewind when skipping keys in keylist
    Fixes
    [this](https://github.com/basho/riak_cs/pull/655#issuecomment-23309088)
    issue comment. When skipping past a common prefix, we used to mistakenly
    skip past only the last common prefix in the state record. There could,
    however, be a later-sorted common prefix in the result set. So we now
    skip past the exact common prefix that the last result is (if it is
    one), instead of the last common prefix in the common prefixes field.

--- src/riak_cs_list_objects_fsm_v2.erl
+++ src/riak_cs_list_objects_fsm_v2.erl
@@ -346,14 +346,34 @@ handle_prefix(Key, Prefix, Delimiter) ->
             false
     end.
 
+-spec common_prefix_from_key(Key :: binary(),
+                             Prefix :: binary(),
+                             Delimiter :: binary()) ->
+    binary().
+%% @doc Extract common prefix from `Key'. `Key' must contain `Delimiter', so
+%% you must first check with `key_is_common_prefix'.
+common_prefix_from_key(Key, Prefix, Delimiter) ->
+    RestKey = case Prefix of
+        undefined ->
+            Key;
+        _Prefix ->
+            PrefixLen = byte_size(Prefix),
+            << Prefix:PrefixLen/binary, Rest/binary >> = Key,
+            Rest
+    end,
+    riak_cs_list_objects_utils:extract_group(RestKey, Delimiter).
+
+
 -spec make_start_key(state()) -> binary().
 make_start_key(#state{object_list_ranges=[], req=Request}) ->
     make_start_key_from_marker(Request);
 make_start_key(State=#state{object_list_ranges=PrevRanges,
-                            common_prefixes=CommonPrefixes}) ->
-    case last_result_is_common_prefix(State) andalso CommonPrefixes =/= [] of
+                            req=?LOREQ{prefix=Prefix,
+                                       delimiter=Delimiter}}) ->
+    case last_result_is_common_prefix(State) of
         true ->
-            LastPrefix = lists:last(lists:sort(ordsets:to_list(CommonPrefixes))),
+            Key = element(2, lists:last(PrevRanges)),
+            LastPrefix = common_prefix_from_key(Key, Prefix, Delimiter),
             skip_past_prefix_and_delimiter(LastPrefix);
         false ->
             element(2, lists:last(PrevRanges))
