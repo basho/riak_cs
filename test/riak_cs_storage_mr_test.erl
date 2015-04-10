6851990540980e50e27bbcab311e0da4c1bb7fb5
Shunichi Shinohara <shino@basho.com>
2015-04-10 15:14:36 +0900

    Add unit test for detailed storage calc M/R functions

--- test/riak_cs_storage_mr_test.erl
+++ test/riak_cs_storage_mr_test.erl
@@ -30,6 +30,134 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+-define(M, (m())?MANIFEST).
+-define(PM, (pm())?PART_MANIFEST).
+
+bucket_summary_map_test_() ->
+    DivPoint = {1400, 0, 0},
+    CBB = fun(K, Ms) ->
+                  History = [{M?MANIFEST.uuid, M} || M <- Ms],
+                  [Res] = riak_cs_storage_mr:bucket_summary_map(
+                            riak_object:new(History),
+                            kd, [do_prereduce, {div_point, DivPoint}]),
+                  {proplists:get_value({K, ct}, Res),
+                   proplists:get_value({K, by}, Res),
+                   proplists:get_value({K, bl}, Res)} end,
+    MostActive = ?M{state=active, content_length=mb(10)},
+    LessActive = ?M{state=active, content_length=mb(20),
+                    write_start_time={1000, 0, 0}},
+    NewWriting   = ?M{state=writing, content_length=mb(10),
+                      write_start_time={1500, 0, 0}},
+    OldWriting   = ?M{state=writing, content_length=mb(10),
+                      write_start_time={1300, 0, 0}},
+    NewPd   = ?M{state=pending_delete, content_length=mb(10),
+                 delete_marked_time={1500, 0, 0}},
+    OldPd   = ?M{state=pending_delete, content_length=mb(10),
+                 delete_marked_time={1300, 0, 0}},
+    NewSd = NewPd?MANIFEST{state=scheduled_delete},
+    OldSd = OldPd?MANIFEST{state=scheduled_delete},
+    MpM = ?MULTIPART_MANIFEST{parts=[?PM{content_length=mb(30)},
+                                     ?PM{content_length=mb(30)},
+                                     ?PM{content_length=40}]},
+    MostActiveMP = ?M{state=active, content_length=mb(60)+40, props=[{multipart, MpM}]},
+    LessActiveMP = ?M{state=active, content_length=mb(60)+40,
+                     write_start_time={1000, 0, 0},
+                     props=[{multipart, MpM}]},
+    NewWritingMP = ?M{state=writing, write_start_time={1500, 0, 0},
+                      props=[{multipart, MpM}]},
+    OldWritingMP = ?M{state=writing, write_start_time={1300, 0, 0},
+                      props=[{multipart, MpM}]},
+    NewPdMP = ?M{state=pending_delete, delete_marked_time={1500, 0, 0},
+                 props=[{multipart, MpM}]},
+    OldPdMP = ?M{state=pending_delete, delete_marked_time={1300, 0, 0},
+                 props=[{multipart, MpM}]},
+    NewSdMP = NewPdMP?MANIFEST{state=scheduled_delete},
+    OldSdMP = OldPdMP?MANIFEST{state=scheduled_delete},
+    Combination = [MostActive, LessActiveMP, NewWriting, OldWriting,
+                   OldWritingMP, NewPd, OldPdMP, NewPd, OldPdMP,
+                   NewSd, OldSd, NewSdMP, OldSdMP],
+    [
+     %% Empty
+     ?_assertEqual([],
+                   [{K, V} || {K, V} <- riak_cs_storage_mr:bucket_summary_map(
+                                          riak_object:new([]),
+                                          kd, [do_prereduce, {div_point, DivPoint}]),
+                              V =/= 0]),
+
+     %% Active only
+     ?_assertEqual({1, mb(10),    10}, CBB(user, [MostActive])),
+     ?_assertEqual({3, mb(60)+40, 61}, CBB(user, [MostActiveMP])),
+     %% Old active
+     ?_assertEqual({1, mb(10),    10}, CBB(user,  [MostActive, LessActive])),
+     ?_assertEqual({1, mb(20),    20}, CBB(ac_de, [MostActive, LessActive])),
+     ?_assertEqual({1, mb(10),    10}, CBB(user,  [MostActive, LessActiveMP])),
+     ?_assertEqual({3, mb(60)+40, 61}, CBB(ac_de, [MostActive, LessActiveMP])),
+
+     %% Writing MP
+     ?_assertEqual({3, mb( 60)+40,  61}, CBB(wr_mp,  [NewWritingMP])),
+     ?_assertEqual({3, mb( 60)+40,  61}, CBB(user,   [NewWritingMP])),
+     ?_assertEqual({3, mb( 60)+40,  61}, CBB(wr_mp,  [OldWritingMP])),
+     ?_assertEqual({3, mb( 60)+40,  61}, CBB(user,   [OldWritingMP])),
+     ?_assertEqual({6, mb(120)+80, 122}, CBB(wr_mp,  [NewWritingMP, OldWritingMP])),
+     ?_assertEqual({6, mb(120)+80, 122}, CBB(user,   [NewWritingMP, OldWritingMP])),
+     ?_assertEqual({0,          0,   0}, CBB(wr_new, [NewWritingMP, OldWritingMP])),
+     ?_assertEqual({0,          0,   0}, CBB(wr_old, [NewWritingMP, OldWritingMP])),
+
+     %% Writing non-MP
+     ?_assertEqual({1, mb(10),    10}, CBB(wr_new, [NewWriting])),
+     ?_assertEqual({1, mb(10),    10}, CBB(wr_old, [OldWriting])),
+     ?_assertEqual({1, mb(10),    10}, CBB(wr_new, [NewWriting, OldWriting])),
+     ?_assertEqual({1, mb(10),    10}, CBB(wr_old, [NewWriting, OldWriting])),
+     %% PD
+     ?_assertEqual({1, mb(10),    10}, CBB(pd_new, [NewPd])),
+     ?_assertEqual({1, mb(10),    10}, CBB(pd_old, [OldPd])),
+     ?_assertEqual({3, mb(60)+40, 61}, CBB(pd_new, [NewPdMP])),
+     ?_assertEqual({3, mb(60)+40, 61}, CBB(pd_old, [OldPdMP])),
+     %% SD
+     ?_assertEqual({1, mb(10),    10}, CBB(sd_new, [NewSd])),
+     ?_assertEqual({1, mb(10),    10}, CBB(sd_old, [OldSd])),
+     ?_assertEqual({3, mb(60)+40, 61}, CBB(sd_new, [NewSdMP])),
+     ?_assertEqual({3, mb(60)+40, 61}, CBB(sd_old, [OldSdMP])),
+
+     %% Combination
+     ?_assertEqual({4, mb(70)+40,   71}, CBB(user,   Combination)),
+     ?_assertEqual({3, mb(60)+40,   61}, CBB(ac_de,  Combination)),
+     ?_assertEqual({3, mb(60)+40,   61}, CBB(wr_mp,  Combination)),
+     ?_assertEqual({1, mb(10),      10}, CBB(wr_new, Combination)),
+     ?_assertEqual({2, mb(20),      20}, CBB(pd_new, Combination)),
+     ?_assertEqual({6, mb(120)+80, 122}, CBB(pd_old, Combination)),
+     ?_assertEqual({4, mb(70)+40,   71}, CBB(sd_new, Combination)),
+     ?_assertEqual({4, mb(70)+40,   71}, CBB(sd_old, Combination))
+    ].
+
+m() ->
+    ?MANIFEST{uuid=druuid:v4(),
+              content_length=0,
+              write_start_time={1200, 0, 0},
+              block_size=riak_cs_lfs_utils:block_size()}.
+
+pm() ->
+    ?PART_MANIFEST{block_size=riak_cs_lfs_utils:block_size()}.
+
+mb(MB) -> MB * 1024 * 1024.
+
+bucket_summary_reduce_test() ->
+    ?assertEqual([[]],
+                 riak_cs_storage_mr:bucket_summary_reduce([[]], args)),
+    ?assertEqual([[{a, 1}]],
+                 riak_cs_storage_mr:bucket_summary_reduce([[{a, 1}]], args)),
+    ?assertEqual([[{a, 1}, {b, 1}]],
+                 riak_cs_storage_mr:bucket_summary_reduce([[{a, 1}, {b, 1}]], args)),
+    ?assertEqual([[{a, 1}, {b, 1}]],
+                 riak_cs_storage_mr:bucket_summary_reduce([[{a, 1}],
+                                                           [{b, 1}]], args)),
+    ?assertEqual([[{a, 3}]],
+                 riak_cs_storage_mr:bucket_summary_reduce([[{a, 1}],
+                                                           [{a, 2}]], args)),
+    ?assertEqual([[{a, 3}, {b, 6}]],
+                 riak_cs_storage_mr:bucket_summary_reduce([[{a, 1}, {b, 1}],
+                                                           [{a, 2}, {b, 5}]], args)).
+
 object_size_map_test_() ->
     M0 = ?MANIFEST{state=active, content_length=25},
     M1 = ?MANIFEST{state=active, content_length=35},
