b2ea65840f3d23f1feb3d880b329a9ed07fa3773
Shunichi Shinohara <shino@basho.com>
2015-07-27 16:50:51 +0900

    Fix typo that prevents proper NoSuchUpload response

--- src/riak_cs_wm_object_upload_part.erl
+++ src/riak_cs_wm_object_upload_part.erl
@@ -284,7 +284,7 @@ accept_body(RD, Ctx0=#context{local_context=LocalCtx0,
                                     riak_cs_riak_client:checkin(ReadRcPid)
                                 end
                         end;
-                    {error, notfond} ->
+                    {error, notfound} ->
                         riak_cs_s3_response:no_such_upload_response(UploadId, RD, Ctx0);
                     {error, Reason} ->
                         riak_cs_s3_response:api_error(Reason, RD, Ctx0)
