60c6ae88a800eeeae94c709e81841dcefb156d67
Shunichi Shinohara <shino@basho.com>
2015-08-03 12:24:23 +0900

    Update aws-sdk-java and other library versions

--- client_tests/clojure/clj-s3/project.clj
+++ client_tests/clojure/clj-s3/project.clj
@@ -1,9 +1,16 @@
-(defproject java-s3-tests/java-s3-tests "0.0.1"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.reiddraper/clj-aws-s3 "0.4.2"]
-                 [clj-http "0.7.1"]
+(defproject java-s3-tests/java-s3-tests "0.0.2"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 ;; aws-sdk-java deps
+                 [com.amazonaws/aws-java-sdk "1.10.8" :exclusions [joda-time]]
+                 [joda-time "2.8.1"]
+                 [com.fasterxml.jackson.core/jackson-core "2.5.3"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.5.3"]
+                 ;; user_creation deps
+                 [clj-http "2.0.0"]
                  [cheshire "5.1.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+  ;; :jvm-opts ["-verbose:class"]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]}}
   :min-lein-version "2.0.0"
-  :plugins [[lein-midje "3.0.1"]]
+  :plugins [[lein-midje "3.1.3"]]
   :description "integration tests for Riak CS using the offical Java SDK")
