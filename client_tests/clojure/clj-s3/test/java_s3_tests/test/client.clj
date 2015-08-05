48590cb48bf28e3a547023b1d05893dbcfafcee9
Shunichi Shinohara <shino@basho.com>
2015-08-05 12:19:00 +0900

    Refactor clojure test code

--- client_tests/clojure/clj-s3/test/java_s3_tests/test/client.clj
+++ client_tests/clojure/clj-s3/test/java_s3_tests/test/client.clj
@@ -1,4 +1,4 @@
-;; Copyright (c) 2007-2013 Basho Technologies, Inc.  All Rights Reserved.
+;; Copyright (c) 2007-2015 Basho Technologies, Inc.  All Rights Reserved.
 ;;
 ;; This file is provided to you under the Apache License,
 ;; Version 2.0 (the "License"); you may not use this file
@@ -24,12 +24,8 @@
   (:require [clojure.tools.logging :as log])
   (:use midje.sweet))
 
-(def ^:internal riak-cs-host-with-protocol "http://localhost")
-(def ^:internal riak-cs-host "localhost")
-
-(defn input-stream-from-string
-  [s]
-  (java.io.ByteArrayInputStream. (.getBytes s "UTF-8")))
+(def ^:internal riak-cs-host "127.0.0.1")
+(def ^:internal riak-cs-host-with-protocol (str "http://" riak-cs-host))
 
 (defn get-riak-cs-port-str
   "Try to get a TCP port number from the OS environment"
@@ -58,25 +54,31 @@
    :proxy {:host riak-cs-host
            :port (get-riak-cs-port)}})
 
+(defn random-user
+  []
+  (user-creation/create-random-user riak-cs-host-with-protocol (get-riak-cs-port)))
+
+(defmacro with-random-user
+  "Execute `body` in implicit do with a random-user bound to `var-name`"
+  [var-name & body]
+  `(let [~var-name (random-user)]
+     (do ~@body)))
+
 (defn random-cred
   []
-  (let [new-cred (user-creation/create-random-user
-                   riak-cs-host-with-protocol
-                   (get-riak-cs-port))]
-    (user-to-cred new-cred)))
+  (user-to-cred (random-user)))
 
 (defmacro with-random-cred
-  "Execute `form` with a random-cred
-  bound to `var-name`"
-  [var-name form]
+  "Execute `body` in implicit do with a random-cred bound to `var-name`"
+  [var-name & body]
   `(let [~var-name (random-cred)]
-     ~form))
+     (do ~@body)))
 
 (defn random-string []
   (str (java.util.UUID/randomUUID)))
 
 (defn write-file [filename content]
-  (with-open [w (clojure.java.io/writer  filename :append false)]
+  (with-open [w (clojure.java.io/writer filename :append false)]
     (.write w content)))
 
 (defn etag-suffix [etag]
@@ -99,32 +101,30 @@
       => [])
 
 (let [bucket-name (random-string)]
-  (fact "creating a bucket should list
-        one bucket in list buckets"
+  (fact "creating a bucket should list one bucket in list buckets"
         (with-random-cred c
-          (do (s3/create-bucket c bucket-name)
-              ((comp :name first) (s3/list-buckets c))))
-        => bucket-name))
+          (s3/create-bucket c bucket-name)
+          (map :name (s3/list-buckets c))
+        => [bucket-name])))
 
 (let [bucket-name (random-string)
       object-name (random-string)]
   (fact "simple put works"
         (with-random-cred cred
-          (do (s3/create-bucket cred bucket-name)
-              (s3/put-object cred bucket-name object-name
-                             "contents")))
+          (s3/create-bucket cred bucket-name)
+          (s3/put-object cred bucket-name object-name
+                         "contents"))
         => truthy))
 
 (let [bucket-name (random-string)
       object-name (random-string)
       value "this is the value!"]
   (fact "the value received during GET is the same
-        as the object that was PUT"
+         as the object that was PUT"
         (with-random-cred c
-          (do (s3/create-bucket c bucket-name)
-              (s3/put-object c bucket-name object-name
-                             value)
-              ((comp slurp :content) (s3/get-object c bucket-name object-name))))
+          (s3/create-bucket c bucket-name)
+          (s3/put-object c bucket-name object-name value)
+          ((comp slurp :content) (s3/get-object c bucket-name object-name)))
         => value))
 
 (let [bucket-name (random-string)
@@ -132,33 +132,31 @@
       value "this is the value!"
       as-bytes (.getBytes value "UTF-8")
       md5-sum (md5-string as-bytes)]
-  (fact "check that the etag of the response
-        is the same as the md5 of the original
-        object"
+  (fact "check that the etag of the response is the same as the md5
+         of the original object"
         (with-random-cred c
-          (do (s3/create-bucket c bucket-name)
-            (s3/put-object c bucket-name object-name
-                           value)
-            ((comp :etag :metadata)
-               (s3/get-object
-                 c bucket-name object-name))))
+          (s3/create-bucket c bucket-name)
+          (s3/put-object c bucket-name object-name value)
+          ((comp :etag :metadata)
+           (s3/get-object
+            c bucket-name object-name)))
         => md5-sum))
 
 (let [bucket-name (random-string)
       object-name (random-string)
-      value (str "aaaaaaaaaa" "bbbbbbbbbb")
+      value "aaaaaaaaaabbbbbbbbbb"
       file-name "./clj-mp-test.txt"]
   (fact "multipart upload works"
         (with-random-cred c
-          (do
-            (s3/create-bucket c bucket-name)
-            (write-file file-name value)
-            (upload-file c bucket-name object-name file-name 10)
-            (let [fetched-object (s3/get-object
-                                  c bucket-name object-name)]
-              [((comp slurp :content) fetched-object)
-               ((comp etag-suffix :etag :metadata) fetched-object)])))
-        => [value, "-2"]))
+          (s3/create-bucket c bucket-name)
+          (write-file file-name value)
+          (upload-file c bucket-name object-name file-name 10)
+          (let [fetched-object (s3/get-object
+                                c bucket-name object-name)]
+            ((comp slurp :content) fetched-object)
+            => value
+            ((comp etag-suffix :etag :metadata) fetched-object)
+            => "-2"))))
 
 (let [bucket-name (random-string)
       object-name (random-string)
@@ -166,10 +164,9 @@
       wrong-md5 "2945d7de2f70de5b8c0cb3fbcba4fe92"]
   (fact "Bad content md5 throws an exception"
         (with-random-cred c
-          (do
-            (s3/create-bucket c bucket-name)
-            (s3/put-object c bucket-name object-name value
-                           {:content-md5 wrong-md5})))
+          (s3/create-bucket c bucket-name)
+          (s3/put-object c bucket-name object-name value
+                         {:content-md5 wrong-md5}))
         => (throws AmazonS3Exception)))
 
 (def bad-canonical-id
@@ -181,11 +178,9 @@
   (fact "Nonexistent canonical-id grant header returns HTTP 400 on
         a put object request (not just an ACL subresource request)"
         (with-random-cred c
-          (do
-            ;; create a bucket
-            (s3/create-bucket c bucket-name)
-            (s3/put-object c bucket-name object-name value-string {}
-                           (s3/grant {:id bad-canonical-id} :full-control))))
+          (s3/create-bucket c bucket-name)
+          (s3/put-object c bucket-name object-name value-string {}
+                         (s3/grant {:id bad-canonical-id} :full-control)))
         => (throws AmazonS3Exception)))
 
 (let [bucket-name (random-string)
@@ -195,15 +190,11 @@
   (fact "Creating an object with an ACL returns the same ACL when you read
         the ACL"
         (with-random-cred c
-          (do
-            ;; create a bucket
-            (s3/create-bucket c bucket-name)
-            (s3/put-object c bucket-name object-name value-string {}
-                           (s3/grant :all-users :read))
-            (contains?
-             (:grants (s3/get-object-acl c bucket-name object-name))
-             public-read-grant)))
-        => truthy))
+          (s3/create-bucket c bucket-name)
+          (s3/put-object c bucket-name object-name value-string {}
+                         (s3/grant :all-users :read))
+          (:grants (s3/get-object-acl c bucket-name object-name)))
+        => (contains (just public-read-grant))))
 
 (let [bucket-name (random-string)
       object-name (random-string)
@@ -212,68 +203,47 @@
   (fact "Creating an object with an ACL returns the same ACL when you read
         the ACL"
         (with-random-cred c
-          (do
-            ;; create a bucket
-            (s3/create-bucket c bucket-name)
-            (s3/put-object c bucket-name object-name value-string {}
-                           (s3/grant :all-users :read))
-            (contains?
-             (:grants (s3/get-object-acl c bucket-name object-name))
-             public-read-grant)))
-        => truthy))
+          (s3/create-bucket c bucket-name)
+          (s3/put-object c bucket-name object-name value-string {}
+                         (s3/grant :all-users :read))
+          (:grants (s3/get-object-acl c bucket-name object-name)))
+        => (contains (just public-read-grant))))
 
 (let [bucket-name (random-string)
       object-name (random-string)
       value-string "this is the real value"]
-  (fact "Creating an object with an (non-canned) ACL returns the same ACL
-        when you read the ACL"
-        (with-random-cred c
-          (do
+  (with-random-cred c
+    (with-random-user u2
+      (fact "Creating an object with an (non-canned) ACL returns the same ACL
+             when you read the ACL"
             (s3/create-bucket c bucket-name)
-            (let [user-two (user-creation/create-random-user
-                            riak-cs-host-with-protocol
-                            (get-riak-cs-port))
-                  user-two-id (:id user-two)
-                  user-two-name (:display_name user-two)
-                  acl-grant {:grantee {:id user-two-id, :display-name user-two-name},
-                             :permission :read}]
-              (s3/put-object c bucket-name object-name value-string {}
-                             (s3/grant {:id user-two-id} :read))
-              (contains?
-                (:grants (s3/get-object-acl c bucket-name object-name))
-                acl-grant))))
-        => truthy))
+            (s3/put-object c bucket-name object-name value-string {}
+                           (s3/grant {:id (:id u2)} :read))
+            (:grants (s3/get-object-acl c bucket-name object-name))
+            => (contains (just {:grantee {:id (:id u2),
+                                          :display-name (:display_name u2)},
+                                :permission :read}))))))
 
 (let [bucket-name (random-string)
-      object-name (random-string)
-      public-read-grant {:grantee :all-users, :permission :read}]
+      object-name (random-string)]
   (fact "Creating a bucket with an ACL returns the same ACL when you read
         the ACL"
         (with-random-cred c
           (do
             (s3/create-bucket c bucket-name {}
                               (s3/grant :all-users :read))
-            (contains?
-             (:grants (s3/get-bucket-acl c bucket-name))
-             public-read-grant)))
-        => truthy))
+            (:grants (s3/get-bucket-acl c bucket-name))))
+        => (contains (just {:grantee :all-users, :permission :read}))))
 
 (let [bucket-name (random-string)
       object-name (random-string)]
-  (fact "Creating a bucket with an (non-canned) ACL returns the same ACL
-        when you read the ACL"
-        (with-random-cred c
-          (do
-            (let [user-two (user-creation/create-random-user
-                            riak-cs-host-with-protocol
-                            (get-riak-cs-port))
-                  user-two-id (:id user-two)
-                  user-two-name (:display_name user-two)
-                  acl-grant {:grantee {:id user-two-id, :display-name user-two-name},
-                             :permission :write}]
-              (s3/create-bucket c bucket-name {}
-                                (s3/grant {:id user-two-id} :write))
-              (contains?
+  (with-random-cred c
+    (with-random-user u2
+        (fact "Creating a bucket with an (non-canned) ACL returns the same ACL
+               when you read the ACL"
+               (s3/create-bucket c bucket-name {}
+                                 (s3/grant {:id (:id u2)} :write))
                (:grants (s3/get-bucket-acl c bucket-name))
-               acl-grant))))
-        => truthy))
+               => (contains (just {:grantee {:id (:id u2),
+                                             :display-name (:display_name u2)},
+                                   :permission :write}))))))
