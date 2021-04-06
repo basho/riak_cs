cb99c59f1643783a8558a329f0f393fcd6390362
Andrei Zavada <andrei.zavada@tiot.jp>
2021-04-07 00:19:28 +0300

    catch dyntrace calls

--- apps/riak_cs/src/riak_cs_wm_common.erl
+++ apps/riak_cs/src/riak_cs_wm_common.erl
@@ -71,7 +71,7 @@
 
 -spec init([{atom(),term()}]) -> {ok, #context{}}.
 init(Config) ->
-    _ = dyntrace:put_tag(pid_to_list(self())),
+    catch dyntrace:put_tag(pid_to_list(self())),
     Mod = proplists:get_value(submodule, Config),
     riak_cs_dtrace:dt_wm_entry({?MODULE, Mod}, <<"init">>),
     %% Check if authentication is disabled and set that in the context.
