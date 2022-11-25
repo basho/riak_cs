7d12b35031407f31099aad4418097fea0e86c4c9
Andrei Zavada <johnhommer@gmail.com>
2022-11-25 16:44:54 +0200

    bulk update copyright year

--- apps/riak_cs/include/defs.hrl
+++ apps/riak_cs/include/defs.hrl
@@ -1,6 +1,6 @@
 %% ---------------------------------------------------------------------
 %%
-%% Copyright (c) 2021 TI Tokyo    All Rights Reserved.
+%% Copyright (c) 2021, 2022 TI Tokyo    All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
--- apps/riak_cs/src/cs/riak_cs_kv_multi_backend.erl
+++ apps/riak_cs/src/cs/riak_cs_kv_multi_backend.erl
@@ -3,7 +3,7 @@
 %% riak_cs_kv_multi_backend: switching between multiple storage engines
 %%
 %% Copyright (c) 2007-2013 Basho Technologies, Inc.  All Rights Reserved,
-%%               2021 TI Tokyo    All Rights Reserved.
+%%               2021, 2022 TI Tokyo    All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
