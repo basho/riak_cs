26bb2745a533182726b020fe6d27101a8a002703
Andrei Zavada <andrei.zavada@tiot.jp>
2023-03-02 19:09:12 +0200

    fixups in vars.config

--- apps/riak_cs/test/riak_cs_config_test.erl
+++ apps/riak_cs/test/riak_cs_config_test.erl
@@ -30,7 +30,7 @@ default_config_test() ->
     Config = cuttlefish_unit:generate_templated_config(schema_files(), [], context()),
     cuttlefish_unit:assert_config(Config, "riak_cs.listener", {"127.0.0.1", 8080}),
     cuttlefish_unit:assert_config(Config, "riak_cs.riak_host", {"127.0.0.1", 8087}),
-    cuttlefish_unit:assert_config(Config, "riak_cs.stanchion_host", {"127.0.0.1", 8085}),
+    cuttlefish_unit:assert_config(Config, "riak_cs.stanchion_listener", {"127.0.0.1", 8085}),
     cuttlefish_unit:assert_config(Config, "riak_cs.stanchion_ssl", false),
     cuttlefish_unit:assert_not_configured(Config, "riak_cs.ssl"),
     cuttlefish_unit:assert_config(Config, "riak_cs.anonymous_user_creation", false),
